8d641c0b1dda4fe22a59c8bccaadcda67b7ee568
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-09-12 21:56:04 +0200

    useDetectGPU docs

--- src/misc/useDetectGPU.tsx
+++ src/misc/useDetectGPU.tsx
@@ -4,11 +4,20 @@ import { getGPUTier, IGetGPUTier } from 'detect-gpu'
 export function useDetectGPU(
   props: IGetGPUTier
 ): {
+  isDesktop: boolean
+  isMobile: boolean
   tier: string
   type: string
 } {
   const GPUTier = useMemo(() => {
-    return getGPUTier(props)
+    const GPUTier = getGPUTier(props)
+
+    return {
+      isDesktop: GPUTier.tier.indexOf('DESKTOP') > -1,
+      isMobile: GPUTier.tier.indexOf('MOBILE') > -1,
+      tier: GPUTier.tier.split('_').pop(),
+      type: GPUTier.type,
+    }
   }, [props])
 
   return GPUTier

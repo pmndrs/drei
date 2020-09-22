c8ed64a2d0bbead6f0bd40faf3da937ae2b1e5c4
Stephen Corwin <stephenjcorwin@gmail.com>
2020-09-21 23:11:22 -0400

    useContextBridge no longer remounts

--- .storybook/stories/useContextBridge.stories.js
+++ .storybook/stories/useContextBridge.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Canvas} from 'react-three-fiber'
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { withKnobs } from '@storybook/addon-knobs'
 
 import { OrbitControls } from '../../src/controls/OrbitControls'
 import { Box } from '../../src/shapes/generated'
@@ -22,25 +22,28 @@ const ThemeContext = React.createContext({})
 function Scene() {
   // we can now use the context within the canvas via the regular hook
   const theme = React.useContext(ThemeContext)
-  const greeting = React.useContext(GreetingContext)
+  const {greeting, setGreeting} = React.useContext(GreetingContext)
   return <>
     <Box
       position-x={-4}
       args={[3, 2]}
       material-color={theme.colors.red}
+      onClick={() => setGreeting(theme.colors.red)}
     />
     <Box
       position-x={0}
       args={[3, 2]}
       material-color={theme.colors.green}
+      onClick={() => setGreeting(theme.colors.green)}
     />
     <Box
       position-x={4}
       args={[3, 2]}
       material-color={theme.colors.blue}
+      onClick={() => setGreeting(theme.colors.blue)}
     />
 
-    <Text fontSize={0.3} position-z={2}>{greeting}</Text>
+    <Text fontSize={0.3} position-z={2}>{greeting ? `Hello ${greeting}!` : 'Click a color'}</Text>
   </>
 }
 
@@ -51,20 +54,20 @@ function SceneWrapper() {
       {/* create the bridge inside the Canvas and forward the context */}
       <ContextBridge>
         <Scene />
-      </ContextBridge>
       <OrbitControls enablePan={false} zoomSpeed={0.5} />
+      </ContextBridge>
     </Canvas>
   )
 }
 
 function UseContextBridgeStory() {
-  const greeting = text('Greeting', "Hello World");
+  const [greeting, setGreeting] = React.useState(null);
   return (
     // Provide several contexts from above the Canvas
     // This mimics the standard behavior of composing them
     // in the `App.tsx` or `index.tsx` files
     <ThemeContext.Provider value={{colors: {red: '#ff0000', green: '#00ff00', blue: '#0000ff'}}}>
-      <GreetingContext.Provider value={greeting}>
+      <GreetingContext.Provider value={{greeting, setGreeting}}>
         <SceneWrapper />
       </GreetingContext.Provider>
     </ThemeContext.Provider>

f47a34327027b06f0c0b6bd1e4f67957c8af3e5b
Paul Henschel <drcmda@gmail.com>
2020-11-07 13:50:26 +0100

    complete effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,12 +1,20 @@
 import { WebGLMultisampleRenderTarget, RGBAFormat, sRGBEncoding } from 'three'
-import React, { useRef, useEffect, useState } from 'react'
+import React, { forwardRef, useRef, useEffect, useState } from 'react'
 import { ReactThreeFiber, extend, useThree, useFrame } from 'react-three-fiber'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
 import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader'
+import mergeRefs from 'react-merge-refs'
 
 extend({ EffectComposer, RenderPass, ShaderPass })
+
+type Props = ReactThreeFiber.Node<EffectComposer, typeof EffectComposer> & {
+  multisamping?: number
+  renderIndex?: number
+  disableGamma?: boolean
+  disableRenderPass?: boolean
+}
 
 declare global {
   namespace JSX {
@@ -27,32 +35,37 @@ export const isWebGL2Available = () => {
   }
 }
 
-export function Effects({ children, multisamping = 8, renderIndex = 1 }) {
-  const composer = useRef<EffectComposer>()
-  const { scene, camera, gl, size } = useThree()
-  const [target] = useState(() => {
-    if (isWebGL2Available() && multisamping > 0) {
-      const t = new WebGLMultisampleRenderTarget(size.width, size.height, {
-        format: RGBAFormat,
-        encoding: sRGBEncoding,
-      })
-      t.samples = 8
-      return t
-    }
-  })
+export const Effects = forwardRef(
+  (
+    { children, multisamping = 8, renderIndex = 1, disableGamma = false, disableRenderPass = false, ...props }: Props,
+    ref
+  ) => {
+    const composer = useRef<EffectComposer>()
+    const { scene, camera, gl, size } = useThree()
+    const [target] = useState(() => {
+      if (isWebGL2Available() && multisamping > 0) {
+        const t = new WebGLMultisampleRenderTarget(size.width, size.height, {
+          format: RGBAFormat,
+          encoding: sRGBEncoding,
+        })
+        t.samples = 8
+        return t
+      }
+    })
 
-  useEffect(() => {
-    composer.current?.setSize(size.width, size.height)
-    composer.current?.setPixelRatio(gl.getPixelRatio())
-  }, [gl, size])
+    useEffect(() => {
+      composer.current?.setSize(size.width, size.height)
+      composer.current?.setPixelRatio(gl.getPixelRatio())
+    }, [gl, size])
 
-  useFrame(() => composer.current?.render(), renderIndex)
+    useFrame(() => composer.current?.render(), renderIndex)
 
-  return (
-    <effectComposer ref={composer} args={[gl, target]}>
-      <renderPass attachArray="passes" args={[scene, camera]} />
-      <shaderPass attachArray="passes" args={[GammaCorrectionShader]} />
-      {children}
-    </effectComposer>
-  )
-}
+    return (
+      <effectComposer ref={mergeRefs(ref, composer)} args={[gl, target]} {...props}>
+        {!disableRenderPass && <renderPass attachArray="passes" args={[scene, camera]} />}
+        {!disableGamma && <shaderPass attachArray="passes" args={[GammaCorrectionShader]} />}
+        {children}
+      </effectComposer>
+    )
+  }
+)

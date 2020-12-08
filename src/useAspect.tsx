5955f857b5b6a9a5c6a5cc4aeea4e46a92f8168c
tsumo <tsumo@users.noreply.github.com>
2020-12-08 17:54:02 +0300

    Fix types for useAspect (#185)

--- src/useAspect.tsx
+++ src/useAspect.tsx
@@ -1,6 +1,6 @@
 import { useThree } from 'react-three-fiber'
 
-export function useAspect(type: string, width: number, height: number, factor: number = 1) {
+export function useAspect(type: 'cover', width: number, height: number, factor: number = 1): [number, number, number] {
   const { viewport: v, aspect } = useThree()
   const adaptedHeight = height * (aspect > width / height ? v.width / width : v.height / height)
   const adaptedWidth = width * (aspect > width / height ? v.width / width : v.height / height)

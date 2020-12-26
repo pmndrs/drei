c3d5ce04a91882bb0ac43e66b78d60fc1674bf65
Matt Rossman <rossman@gatech.edu>
2020-12-26 16:20:33 -0500

    Add cubemap presets to Environment component (#203)
    * Add preset prop, start on cubemap-assets
    
    * Add Environment story
    
    * Add remaining preset data, show all in story
    
    * Add error message for invalid preset
    
    * Update cubemap presets to point to new assets repo
    
    * Update imports in line w/ esModuleInterop refactor
    
    See ab51212a952c8392e7f270a493ad68965e4248df

--- src/Environment.tsx
+++ src/Environment.tsx
@@ -3,6 +3,7 @@ import { useLoader, useThree } from 'react-three-fiber'
 import { CubeTextureLoader, Texture, PMREMGenerator } from 'three'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 import { useAsset } from 'use-asset'
+import { presetsObj } from './helpers/cubemap-assets'
 
 function getTexture(texture, gen, isCubeMap) {
   if (isCubeMap) {
@@ -12,11 +13,28 @@ function getTexture(texture, gen, isCubeMap) {
   return gen.fromEquirectangular(texture).texture
 }
 
+const CUBEMAP_ROOT = 'https://rawcdn.githack.com/mattrossman/drei-assets/b597559ff62f85ec691df28cbea5ecb1263a2085'
+
+type Props = {
+  background?: boolean
+  files?: string | string[]
+  path?: string
+  preset?: string
+}
+
 export function Environment({
   background = false,
   files = ['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'],
   path = '/',
-}) {
+  preset = undefined,
+}: Props) {
+  if (preset) {
+    if (!(preset in presetsObj)) {
+      throw new Error('Preset must be one of: ' + Object.keys(presetsObj).join(', '))
+    }
+    files = presetsObj[preset]
+    path = CUBEMAP_ROOT + '/hdri/'
+  }
   const { gl, scene } = useThree()
   const isCubeMap = Array.isArray(files)
   const loader: any = isCubeMap ? CubeTextureLoader : RGBELoader

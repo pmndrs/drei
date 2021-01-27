487cb5aa5eabc73aec77768b46d2f0652e4893b5
Jure Triglav <juretriglav@gmail.com>
2021-01-27 17:03:01 +0100

    BREAKING CHANGE: Upgrade Three.js to r125 (#262)
    * chore: update Three.js dependency
    
    * refactor: remove subdivision modifier
    
    * fix: remove use of FaceNormalsHelper as it no longer exists in Three
    
    * fix: update typings for THREE
    
    * fix: update modifiers to work without THREE.Geometry
    
    * docs: update README
    
    * chore: add Three.js peer dependency

--- .storybook/stories/useEdgeSplit.stories.tsx
+++ .storybook/stories/useEdgeSplit.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { useLoader } from 'react-three-fiber'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { withKnobs, number } from '@storybook/addon-knobs'
+import { withKnobs, number, boolean } from '@storybook/addon-knobs'
 
 import { Setup } from '../Setup'
 
@@ -17,8 +17,9 @@ function CerberusModel() {
   const { children } = useLoader(OBJLoader, 'cerberus.obj')
 
   const cutoffEdge = number('Cut Off Edge', 20, { range: true, min: 0, max: 180, step: 1 })
+  const tryKeepNormals = boolean('Keep normals', true)
 
-  const meshRef = useEdgeSplit(cutoffEdge * (Math.PI / 180))
+  const meshRef = useEdgeSplit(cutoffEdge * (Math.PI / 180), tryKeepNormals)
 
   return (
     <mesh scale={[8, 8, 8]} ref={meshRef} geometry={(children[0] as THREE.Mesh).geometry}>

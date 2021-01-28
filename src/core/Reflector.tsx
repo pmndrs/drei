3f6dab6fafeff8d4b0239e43ea3da08914a2acb8
Paul Henschel <drcmda@gmail.com>
2021-01-28 14:26:06 +0100

    repurpose reflector, make it capable to blur (#260)
    * repurpose reflector, make it capable to blur
    
    * refactor: move materials out to reduce noise & fix types
    
    * refactor: merge beta
    
    * fix: add child propType
    
    * refactor: update reflector story & fix children type
    
    * changes
    
    * fix: shuffle types so reflectorProps come from what the material expects
    
    * docs
    
    Co-authored-by: Josh Ellis <joshua.ellis18@gmail.com>

--- src/core/Reflector.tsx
+++ src/core/Reflector.tsx
@@ -1,42 +1,150 @@
 import * as React from 'react'
-import { Mesh } from 'three'
-import { ReactThreeFiber, Overwrite } from 'react-three-fiber'
-import { Reflector as ReflectorImpl, ReflectorOptions } from 'three/examples/jsm/objects/Reflector'
+import {
+  Plane,
+  Vector3,
+  Vector4,
+  Matrix4,
+  PerspectiveCamera,
+  RGBFormat,
+  Mesh,
+  LinearFilter,
+  WebGLRenderTarget,
+} from 'three'
+import { useFrame, useThree } from 'react-three-fiber'
+import { BlurPass } from '../materials/BlurPass'
+import { MeshReflectorMaterialImpl } from '../materials/ReflectorMaterial'
 
-export type Reflector = Overwrite<
-  ReactThreeFiber.Object3DNode<ReflectorImpl, typeof ReflectorImpl>,
-  { children: React.ReactElement<ReactThreeFiber.Object3DNode<Mesh, typeof Mesh>> }
->
+export type ReflectorChildProps = MeshReflectorMaterialImpl
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      reflectorImpl: Reflector
-    }
+export type ReflectorProps = Omit<JSX.IntrinsicElements['mesh'], 'args' | 'children'> & {
+  resolution?: number
+  mixBlur?: number
+  mixStrength?: number
+  blur?: [number, number] | number
+  args?: [number, number]
+  mirror: number
+  children: {
+    (
+      Component: React.ElementType<JSX.IntrinsicElements['meshReflectorMaterial']>,
+      ComponentProps: ReflectorChildProps
+    ): JSX.Element | null
   }
 }
 
-type Props = ReflectorOptions & Omit<JSX.IntrinsicElements['mesh'], 'args'> & { children: React.ReactElement<any> }
+export function Reflector({
+  mixBlur = 0.0,
+  mixStrength = 0.5,
+  resolution = 256,
+  blur = [0, 0],
+  args = [1, 1],
+  mirror,
+  children,
+  ...props
+}: ReflectorProps) {
+  blur = Array.isArray(blur) ? blur : [blur, blur]
+  const hasBlur = blur[0] + blur[1] > 0
+  const meshRef = React.useRef<Mesh>(null!)
+  const [reflectorPlane] = React.useState(() => new Plane())
+  const [normal] = React.useState(() => new Vector3())
+  const [reflectorWorldPosition] = React.useState(() => new Vector3())
+  const [cameraWorldPosition] = React.useState(() => new Vector3())
+  const [rotationMatrix] = React.useState(() => new Matrix4())
+  const [lookAtPosition] = React.useState(() => new Vector3(0, 0, -1))
+  const [clipPlane] = React.useState(() => new Vector4())
+  const [view] = React.useState(() => new Vector3())
+  const [target] = React.useState(() => new Vector3())
+  const [q] = React.useState(() => new Vector4())
+  const [textureMatrix] = React.useState(() => new Matrix4())
+  const [virtualCamera] = React.useState(() => new PerspectiveCamera())
+  const { gl, scene, camera } = useThree()
 
-export const Reflector = React.forwardRef(
-  ({ children, color, textureWidth, textureHeight, clipBias, shader, encoding, ...props }: Props, ref) => {
-    const reflector = React.useMemo(
-      () =>
-        new ReflectorImpl(undefined, {
-          color,
-          textureWidth,
-          textureHeight,
-          clipBias,
-          shader,
-          encoding,
-        }),
-      [clipBias, color, encoding, shader, textureHeight, textureWidth]
-    )
+  const beforeRender = React.useCallback(() => {
+    reflectorWorldPosition.setFromMatrixPosition(meshRef.current.matrixWorld)
+    cameraWorldPosition.setFromMatrixPosition(camera.matrixWorld)
+    rotationMatrix.extractRotation(meshRef.current.matrixWorld)
+    normal.set(0, 0, 1)
+    normal.applyMatrix4(rotationMatrix)
+    view.subVectors(reflectorWorldPosition, cameraWorldPosition)
+    // Avoid rendering when reflector is facing away
+    if (view.dot(normal) > 0) return
+    view.reflect(normal).negate()
+    view.add(reflectorWorldPosition)
+    rotationMatrix.extractRotation(camera.matrixWorld)
+    lookAtPosition.set(0, 0, -1)
+    lookAtPosition.applyMatrix4(rotationMatrix)
+    lookAtPosition.add(cameraWorldPosition)
+    target.subVectors(reflectorWorldPosition, lookAtPosition)
+    target.reflect(normal).negate()
+    target.add(reflectorWorldPosition)
+    virtualCamera.position.copy(view)
+    virtualCamera.up.set(0, 1, 0)
+    virtualCamera.up.applyMatrix4(rotationMatrix)
+    virtualCamera.up.reflect(normal)
+    virtualCamera.lookAt(target)
+    virtualCamera.far = camera.far // Used in WebGLBackground
+    virtualCamera.updateMatrixWorld()
+    virtualCamera.projectionMatrix.copy(camera.projectionMatrix)
+    // Update the texture matrix
+    textureMatrix.set(0.5, 0.0, 0.0, 0.5, 0.0, 0.5, 0.0, 0.5, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 1.0)
+    textureMatrix.multiply(virtualCamera.projectionMatrix)
+    textureMatrix.multiply(virtualCamera.matrixWorldInverse)
+    textureMatrix.multiply(meshRef.current.matrixWorld)
+    // Now update projection matrix with new clip plane, implementing code from: http://www.terathon.com/code/oblique.html
+    // Paper explaining this technique: http://www.terathon.com/lengyel/Lengyel-Oblique.pdf
+    reflectorPlane.setFromNormalAndCoplanarPoint(normal, reflectorWorldPosition)
+    reflectorPlane.applyMatrix4(virtualCamera.matrixWorldInverse)
+    clipPlane.set(reflectorPlane.normal.x, reflectorPlane.normal.y, reflectorPlane.normal.z, reflectorPlane.constant)
+    const projectionMatrix = virtualCamera.projectionMatrix
+    q.x = (Math.sign(clipPlane.x) + projectionMatrix.elements[8]) / projectionMatrix.elements[0]
+    q.y = (Math.sign(clipPlane.y) + projectionMatrix.elements[9]) / projectionMatrix.elements[5]
+    q.z = -1.0
+    q.w = (1.0 + projectionMatrix.elements[10]) / projectionMatrix.elements[14]
+    // Calculate the scaled plane vector
+    clipPlane.multiplyScalar(2.0 / clipPlane.dot(q))
+    // Replacing the third row of the projection matrix
+    projectionMatrix.elements[2] = clipPlane.x
+    projectionMatrix.elements[6] = clipPlane.y
+    projectionMatrix.elements[10] = clipPlane.z + 1.0
+    projectionMatrix.elements[14] = clipPlane.w
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-    return (
-      <primitive dispose={undefined} object={reflector} ref={ref as React.MutableRefObject<Mesh>} {...props}>
-        {React.Children.only(children)}
-      </primitive>
-    )
-  }
-)
+  const [fbo1, fbo2, blurpass, reflectorProps] = React.useMemo(() => {
+    const parameters = {
+      minFilter: LinearFilter,
+      magFilter: LinearFilter,
+      format: RGBFormat,
+      encoding: gl.outputEncoding,
+    }
+    const fbo1 = new WebGLRenderTarget(resolution, resolution, parameters)
+    const fbo2 = new WebGLRenderTarget(resolution, resolution, parameters)
+    const blurpass = new BlurPass({ gl, resolution, width: blur[0], height: blur[1] })
+    const reflectorProps = {
+      mirror,
+      textureMatrix,
+      mixBlur,
+      tDiffuse: fbo1.texture,
+      tDiffuseBlur: fbo2.texture,
+      hasBlur,
+      mixStrength,
+    }
+    return [fbo1, fbo2, blurpass, reflectorProps]
+  }, [gl, blur, textureMatrix, resolution, mirror, hasBlur, mixBlur, mixStrength])
+
+  useFrame(() => {
+    meshRef.current.visible = false
+    beforeRender()
+    gl.setRenderTarget(fbo1)
+    gl.render(scene, virtualCamera)
+    if (hasBlur) blurpass.render(gl, fbo1, fbo2)
+    meshRef.current.visible = true
+    gl.setRenderTarget(null)
+  })
+
+  return (
+    <mesh ref={meshRef} {...props}>
+      <planeBufferGeometry args={args} />
+      {children ? children('meshReflectorMaterial', reflectorProps) : <meshReflectorMaterial {...reflectorProps} />}
+    </mesh>
+  )
+}
---
+++ src/materials/BlurPass.tsx
@@ -0,0 +1,79 @@
+import {
+  Mesh,
+  BufferGeometry,
+  BufferAttribute,
+  Camera,
+  LinearFilter,
+  Scene,
+  WebGLRenderTarget,
+  WebGLRenderer,
+} from 'three'
+
+import { ConvolutionMaterial } from './ConvolutionMaterial'
+
+export interface BlurPassProps {
+  gl: WebGLRenderer
+  resolution: number
+  width?: number
+  height?: number
+}
+
+export class BlurPass {
+  readonly renderTargetA: WebGLRenderTarget
+  readonly renderTargetB: WebGLRenderTarget
+  readonly convolutionMaterial: ConvolutionMaterial
+  readonly scene: Scene
+  readonly camera: Camera
+  readonly screen: Mesh
+  renderToScreen: boolean = false
+
+  constructor({ gl, resolution, width = 500, height = 500 }: BlurPassProps) {
+    this.renderTargetA = new WebGLRenderTarget(resolution, resolution, {
+      minFilter: LinearFilter,
+      magFilter: LinearFilter,
+      stencilBuffer: false,
+      depthBuffer: false,
+      encoding: gl.outputEncoding,
+    })
+    this.renderTargetB = this.renderTargetA.clone()
+    this.convolutionMaterial = new ConvolutionMaterial()
+    this.convolutionMaterial.setTexelSize(1.0 / width, 1.0 / height)
+    this.scene = new Scene()
+    this.camera = new Camera()
+    const vertices = new Float32Array([-1, -1, 0, 3, -1, 0, -1, 3, 0])
+    const uvs = new Float32Array([0, 0, 2, 0, 0, 2])
+    const geometry = new BufferGeometry()
+    geometry.setAttribute('position', new BufferAttribute(vertices, 3))
+    geometry.setAttribute('uv', new BufferAttribute(uvs, 2))
+    this.screen = new Mesh(geometry, this.convolutionMaterial)
+    this.screen.frustumCulled = false
+    this.scene.add(this.screen)
+  }
+
+  render(renderer, inputBuffer, outputBuffer) {
+    const scene = this.scene
+    const camera = this.camera
+    const renderTargetA = this.renderTargetA
+    const renderTargetB = this.renderTargetB
+    let material = this.convolutionMaterial
+    let uniforms = material.uniforms
+    const kernel = material.kernel
+    let lastRT = inputBuffer
+    let destRT
+    let i, l
+    // Apply the multi-pass blur.
+    for (i = 0, l = kernel.length - 1; i < l; ++i) {
+      // Alternate between targets.
+      destRT = (i & 1) === 0 ? renderTargetA : renderTargetB
+      uniforms.kernel.value = kernel[i]
+      uniforms.inputBuffer.value = lastRT.texture
+      renderer.setRenderTarget(destRT)
+      renderer.render(scene, camera)
+      lastRT = destRT
+    }
+    uniforms.kernel.value = kernel[i]
+    uniforms.inputBuffer.value = lastRT.texture
+    renderer.setRenderTarget(this.renderToScreen ? null : outputBuffer)
+    renderer.render(scene, camera)
+  }
+}

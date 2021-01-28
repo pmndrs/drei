c2840c21356e877f39d371fd0730f57cb7507b5d
Josh Ellis <joshua.ellis18@gmail.com>
2021-01-28 12:37:34 +0000

    refactor: add push to update pkg on release

--- release.config.js
+++ release.config.js
@@ -6,5 +6,12 @@ module.exports = {
         pkgRoot: './dist',
       },
     ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 }

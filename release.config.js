e5c8b46b384bc14e6ab85f77c1707722ebf8af41
Josh Ellis <joshua.ellis18@gmail.com>
2021-01-29 13:32:59 +0000

    chore: remove @semantic-release git config

--- release.config.js
+++ release.config.js
@@ -6,12 +6,5 @@ module.exports = {
         pkgRoot: './dist',
       },
     ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
   ],
 }

ab2231fc58ef98e7ca9c43ff22b24f69e51681a0
Jure Triglav <juretriglav@gmail.com>
2021-02-03 13:46:05 +0100

    refactor: simplify and extract internal properties like TransformControls

--- src/core/PointerLockControls.tsx
+++ src/core/PointerLockControls.tsx
@@ -1,37 +1,31 @@
 import * as React from 'react'
-import { ReactThreeFiber, useThree, Overwrite } from 'react-three-fiber'
+import { ReactThreeFiber, useThree } from 'react-three-fiber'
 import { PointerLockControls as PointerLockControlsImpl } from 'three/examples/jsm/controls/PointerLockControls'
 import useEffectfulState from '../helpers/useEffectfulState'
 
-export type PointerLockControls = Overwrite<
-  ReactThreeFiber.Object3DNode<PointerLockControlsImpl, typeof PointerLockControlsImpl>,
-  { selector?: string }
->
+export type PointerLockControls = ReactThreeFiber.Object3DNode<PointerLockControlsImpl, typeof PointerLockControlsImpl>
 
-export const PointerLockControls = React.forwardRef((props: PointerLockControls, ref) => {
-  const { camera, gl, invalidate } = useThree()
-  const controls = useEffectfulState(
-    () => new PointerLockControlsImpl(camera, gl.domElement),
-    [camera, gl.domElement],
-    ref as any
-  )
+export const PointerLockControls = React.forwardRef(
+  ({ selector, ...props }: { selector: string } & PointerLockControls, ref) => {
+    const { camera, gl, invalidate } = useThree()
+    const controls = useEffectfulState(
+      () => new PointerLockControlsImpl(camera, gl.domElement),
+      [camera, gl.domElement],
+      ref as any
+    )
 
-  // Only props that are not 'selector' are passed down to the primitive
-  // 'selector' is used here and other props are used on Three.js' side
-  const internalProps = (({ selector: _, ...rest }) => rest)(props)
+    React.useEffect(() => {
+      controls?.addEventListener?.('change', invalidate)
+      return () => controls?.removeEventListener?.('change', invalidate)
+    }, [controls, invalidate])
 
-  React.useEffect(() => {
-    controls?.addEventListener?.('change', invalidate)
-    return () => controls?.removeEventListener?.('change', invalidate)
-  }, [controls, invalidate])
+    React.useEffect(() => {
+      const handler = () => controls?.lock()
+      const element = selector ? document.querySelector(selector) : document
+      element && element.addEventListener('click', handler)
+      return () => (element ? element.removeEventListener('click', handler) : undefined)
+    }, [controls, selector])
 
-  React.useEffect(() => {
-    const handler = () => controls?.lock()
-    const selector = props.selector
-    const element = selector ? document.querySelector(selector) : document
-    element && element.addEventListener('click', handler)
-    return () => (element ? element.removeEventListener('click', handler) : undefined)
-  }, [controls, props.selector])
-
-  return controls ? <primitive dispose={undefined} object={controls} {...internalProps} /> : null
-})
+    return controls ? <primitive dispose={undefined} object={controls} {...props} /> : null
+  }
+)

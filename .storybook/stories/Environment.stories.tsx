db071f94bf01c2e2079dee38ac020d0fde72dba0
jbixon13 <jbixon13@gmail.com>
2021-02-02 22:37:06 -0500

    refactor: replace environment knobs with controls

--- .storybook/stories/Environment.stories.tsx
+++ .storybook/stories/Environment.stories.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Vector3 } from 'three'
-import { withKnobs, select, boolean } from '@storybook/addon-knobs'
 
 import { Setup } from '../Setup'
 
@@ -11,23 +10,28 @@ import { presetsObj } from '../../src/helpers/environment-assets'
 export default {
   title: 'Abstractions/Environment',
   component: Environment,
+  argTypes: {
+    preset: {
+      control: {
+        type: 'select',
+        options: Object.keys(presetsObj),
+      },
+    },
+  },
   decorators: [
     (storyFn) => (
       <Setup cameraPosition={new Vector3(0, 0, 10)} controls={false}>
         {storyFn()}
       </Setup>
     ),
-    withKnobs,
   ],
 }
 
-function EnvironmentStory() {
-  const presets = Object.keys(presetsObj)
-  const preset = select('Preset', presets, presets[0])
+function EnvironmentStory({ cfg }) {
   return (
     <>
       <React.Suspense fallback={null}>
-        <Environment preset={preset} background={boolean('Background', true)} />
+        <Environment preset={cfg.preset} background={cfg.background} />
         <mesh>
           <torusKnotBufferGeometry args={[1, 0.5, 128, 32]} />
           <meshStandardMaterial metalness={1} roughness={0} />
@@ -38,5 +42,11 @@ function EnvironmentStory() {
   )
 }
 
-export const EnvironmentSt = () => <EnvironmentStory />
+const controlsConfig = {
+  preset: Object.keys(presetsObj)[0],
+  background: true,
+}
+
+export const EnvironmentSt = ({ ...args }) => <EnvironmentStory cfg={args} />
 EnvironmentSt.storyName = 'Default'
+EnvironmentSt.args = { ...controlsConfig }

87840ff180dca00fecf60489cc7376746963bad5
jbixon13 <jbixon13@gmail.com>
2021-02-07 15:27:48 -0500

    refactor: replace text knobs with controls

--- .storybook/stories/Text.stories.tsx
+++ .storybook/stories/Text.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { DoubleSide, Vector3 } from 'three'
-import { withKnobs, number, color as colorKnob } from '@storybook/addon-knobs'
+import { number, color as colorKnob } from '@storybook/addon-knobs'
 
 import { Setup } from '../Setup'
 import { useTurntable } from '../useTurntable'
@@ -10,24 +10,143 @@ import { Text } from '../../src'
 export default {
   title: 'Abstractions/Text',
   component: Text,
-  decorators: [withKnobs, (storyFn) => <Setup cameraPosition={new Vector3(0, 0, 200)}>{storyFn()}</Setup>],
+  argTypes: {
+    color: {
+      control: {
+        type: 'color',
+      },
+    },
+    opacity: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 1,
+        step: 0.1,
+      },
+    },
+    fontSize: {
+      control: {
+        type: 'number',
+      },
+    },
+    maxWidth: {
+      control: {
+        type: 'number',
+      },
+    },
+    textAlign: {
+      control: {
+        type: 'select',
+        options: ['left', 'center', 'right', 'justify'],
+      },
+    },
+    anchorX: {
+      control: {
+        type: 'select',
+        options: ['left', 'center', 'right'],
+      },
+    },
+    anchorY: {
+      control: {
+        type: 'select',
+        options: ['top', 'top-baseline', 'middle', 'bottom-baseline', 'bottom'],
+      },
+    },
+    letterSpacing: {
+      control: {
+        type: 'number',
+        step: 0.01,
+      },
+    },
+    lineHeight: {
+      control: {
+        type: 'number',
+        step: 0.1,
+      },
+    },
+    outlineColor: {
+      control: {
+        type: 'color',
+      },
+    },
+    outlineWidth: {
+      control: {
+        type: 'number',
+        min: 0,
+        step: 0.2,
+      },
+    },
+    fillOpacity: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 1,
+        step: 0.1,
+      },
+    },
+    strokeWidth: {
+      control: {
+        type: 'number',
+        min: 0,
+        step: 0.1,
+      },
+    },
+    strokeColor: {
+      control: {
+        type: 'color',
+      },
+    },
+    outlineOffsetX: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 10,
+        step: 1,
+      },
+    },
+    outlineOffsetY: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 10,
+        step: 1,
+      },
+    },
+    outlineBlur: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 10,
+        step: 1,
+      },
+    },
+    outlineOpacity: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 1,
+        step: 0.1,
+      },
+    },
+  },
+  decorators: [(storyFn) => <Setup cameraPosition={new Vector3(0, 0, 200)}>{storyFn()}</Setup>],
 }
 
-function TextScene() {
+function TextScene({ cfg }) {
   const ref = useTurntable()
 
   return (
     <Text
       ref={ref}
-      color={'#EC2D2D'}
-      fontSize={12}
-      maxWidth={200}
-      lineHeight={1}
-      letterSpacing={0.02}
-      textAlign={'left'}
+      color={cfg.color}
+      fontSize={cfg.fontSize}
+      maxWidth={cfg.maxWidth}
+      lineHeight={cfg.lineHeight}
+      letterSpacing={cfg.letterSpacing}
+      textAlign={cfg.textAlign}
       font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-      anchorX="center"
-      anchorY="middle"
+      anchorX={cfg.anchorX}
+      anchorY={cfg.anchorY}
     >
       LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
       MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
@@ -37,26 +156,38 @@ function TextScene() {
   )
 }
 
-export const TextSt = () => <TextScene />
+const textControlsConfig = {
+  color: '#ec2d2d',
+  fontSize: 12,
+  maxWidth: 200,
+  lineHeight: 1,
+  letterSpacing: 0.02,
+  textAlign: 'left',
+  anchorX: 'center',
+  anchorY: 'middle',
+}
+
+export const TextSt = ({ ...args }) => <TextScene cfg={args} />
 TextSt.storyName = 'Default'
+TextSt.args = { ...textControlsConfig }
 
-function TextOutlineScene() {
+function TextOutlineScene({ cfg }) {
   const ref = useTurntable()
 
   return (
     <Text
       ref={ref}
-      color={'#EC2D2D'}
-      fontSize={12}
-      maxWidth={200}
-      lineHeight={1}
-      letterSpacing={0.02}
-      textAlign={'left'}
+      color={cfg.color}
+      fontSize={cfg.fontSize}
+      maxWidth={cfg.maxWidth}
+      lineHeight={cfg.lineHeight}
+      letterSpacing={cfg.letterSpacing}
+      textAlign={cfg.textAlign}
       font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-      anchorX="center"
-      anchorY="middle"
-      outlineWidth={2}
-      outlineColor="#ffffff"
+      anchorX={cfg.anchorX}
+      anchorY={cfg.anchorY}
+      outlineWidth={cfg.outlineWidth}
+      outlineColor={cfg.outlineColor}
     >
       LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
       MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
@@ -66,99 +197,152 @@ function TextOutlineScene() {
   )
 }
 
-function TextStrokeScene() {
-  const ref = useTurntable()
-
-  return (
-    <Text
-      ref={ref}
-      fontSize={12}
-      maxWidth={200}
-      lineHeight={1}
-      letterSpacing={0.02}
-      textAlign={'left'}
-      font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-      anchorX="center"
-      anchorY="middle"
-      fillOpacity={0}
-      strokeWidth={'2.5%'}
-      strokeColor="#ffffff"
-    >
-      LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
-      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
-      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
-      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.
-    </Text>
-  )
+const textOutlineControlsConfig = {
+  color: '#ec2d2d',
+  fontSize: 12,
+  maxWidth: 200,
+  lineHeight: 1,
+  letterSpacing: 0.02,
+  textAlign: 'left',
+  anchorX: 'center',
+  anchorY: 'middle',
+  outlineWidth: 2,
+  outlineColor: 'white',
 }
 
-function TextShadowScene() {
-  const ref = useTurntable()
-
-  return (
-    <Text
-      ref={ref}
-      color={'#EC2D2D'}
-      fontSize={12}
-      maxWidth={200}
-      lineHeight={1}
-      letterSpacing={0.02}
-      textAlign={'left'}
-      font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-      anchorX="center"
-      anchorY="middle"
-      outlineOffsetX={'10%'}
-      outlineOffsetY={'10%'}
-      outlineBlur={'30%'}
-      outlineOpacity={0.3}
-      outlineColor="#EC2D2D"
-    >
-      LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
-      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
-      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
-      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.
-    </Text>
-  )
-}
-function CustomMaterialTextScene() {
-  const ref = useTurntable()
-  const defaultColor = '#EC2D2D'
-
-  return (
-    <Text
-      ref={ref}
-      fontSize={12}
-      maxWidth={200}
-      lineHeight={1}
-      letterSpacing={0.02}
-      textAlign={'left'}
-      font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-      anchorX="center"
-      anchorY="middle"
-    >
-      <meshBasicMaterial
-        attach="material"
-        side={DoubleSide}
-        color={colorKnob('Color', defaultColor)}
-        transparent
-        opacity={number('Opacity', 1, { range: true, min: 0, max: 1, step: 0.1 })}
-      />
-      LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
-      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
-      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
-      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.
-    </Text>
-  )
-}
-
-export const TextOutlineSt = () => <TextOutlineScene />
+export const TextOutlineSt = ({ ...args }) => <TextOutlineScene cfg={args} />
 TextOutlineSt.storyName = 'Outline'
+TextOutlineSt.args = { ...textOutlineControlsConfig }
 
-export const TextStrokeSt = () => <TextStrokeScene />
+function TextStrokeScene({ cfg }) {
+  const ref = useTurntable()
+
+  return (
+    <Text
+      ref={ref}
+      fontSize={cfg.fontSize}
+      maxWidth={cfg.maxWidth}
+      lineHeight={cfg.lineHeight}
+      letterSpacing={cfg.letterSpacing}
+      textAlign={cfg.textAlign}
+      font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+      anchorX={cfg.anchorX}
+      anchorY={cfg.anchorY}
+      fillOpacity={cfg.fillOpacity}
+      strokeWidth={cfg.strokeWidth}
+      strokeColor={cfg.strokeColor}
+    >
+      LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
+      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.
+    </Text>
+  )
+}
+
+const textStrokeControlsConfig = {
+  fontSize: 12,
+  maxWidth: 200,
+  lineHeight: 1,
+  letterSpacing: 0.02,
+  textAlign: 'left',
+  anchorX: 'center',
+  anchorY: 'middle',
+  fillOpacity: 0,
+  strokeWidth: 0.25,
+  strokeColor: 'white',
+}
+
+export const TextStrokeSt = ({ ...args }) => <TextStrokeScene cfg={args} />
 TextStrokeSt.storyName = 'Transparent with stroke'
+TextStrokeSt.args = { ...textStrokeControlsConfig }
 
-export const TextShadowSt = () => <TextShadowScene />
+function TextShadowScene({ cfg }) {
+  const ref = useTurntable()
+
+  return (
+    <Text
+      ref={ref}
+      color={cfg.color}
+      fontSize={cfg.fontSize}
+      maxWidth={cfg.maxWidth}
+      lineHeight={cfg.lineHeight}
+      letterSpacing={cfg.letterSpacing}
+      textAlign={cfg.textAlign}
+      font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+      anchorX={cfg.anchorX}
+      anchorY={cfg.anchorY}
+      outlineOffsetX={cfg.outlineOffsetX}
+      outlineOffsetY={cfg.outlineOffsetY}
+      outlineBlur={cfg.outlineBlur}
+      outlineOpacity={cfg.outlineOpacity}
+      outlineColor={cfg.outlineColor}
+    >
+      LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
+      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.
+    </Text>
+  )
+}
+
+const textShadowControlsConfig = {
+  color: '#ec2d2d',
+  fontSize: 12,
+  maxWidth: 200,
+  lineHeight: 1,
+  letterSpacing: 0.02,
+  textAlign: 'left',
+  anchorX: 'center',
+  anchorY: 'middle',
+  outlineOffsetX: 1,
+  outlineOffsetY: 1,
+  outlineBlur: 3,
+  outlineOpacity: 0.3,
+  outlineColor: '#EC2D2D',
+}
+
+export const TextShadowSt = ({ ...args }) => <TextShadowScene cfg={args} />
 TextShadowSt.storyName = 'Text Shadow'
+TextShadowSt.args = { ...textShadowControlsConfig }
 
-export const CustomMaterialTextSt = () => <CustomMaterialTextScene />
+function CustomMaterialTextScene({ cfg }) {
+  const ref = useTurntable()
+  // const defaultColor = '#EC2D2D'
+
+  return (
+    <Text
+      ref={ref}
+      fontSize={cfg.fontSize}
+      maxWidth={cfg.maxWidth}
+      lineHeight={cfg.lineHeight}
+      letterSpacing={cfg.letterSpacing}
+      textAlign={cfg.textAlign}
+      font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+      anchorX={cfg.anchorX}
+      anchorY={cfg.anchorY}
+    >
+      <meshBasicMaterial attach="material" side={DoubleSide} color={cfg.color} transparent opacity={cfg.opacity} />
+      LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
+      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.
+    </Text>
+  )
+}
+
+const customMaterialTextControlsConfig = {
+  color: '#ec2d2d',
+  fontSize: 12,
+  maxWidth: 200,
+  lineHeight: 1,
+  letterSpacing: 0.02,
+  textAlign: 'left',
+  anchorX: 'center',
+  anchorY: 'middle',
+  opacity: 1,
+}
+
+export const CustomMaterialTextSt = ({ ...args }) => <CustomMaterialTextScene cfg={args} />
 CustomMaterialTextSt.storyName = 'Custom Material'
+CustomMaterialTextSt.args = { ...customMaterialTextControlsConfig }

13f1f51e69f0e0c5e69a115ae67d5e783c6d9ed8
nathanbryantdev <78676974+nathanbryantdev@users.noreply.github.com>
2021-02-17 21:24:20 +1100

    Updated HTML Stories with Orthographic Camera

--- .storybook/stories/HTML.stories.tsx
+++ .storybook/stories/HTML.stories.tsx
@@ -4,7 +4,7 @@ import * as THREE from 'three'
 import { Setup } from '../Setup'
 import { useTurntable } from '../useTurntable'
 
-import { Icosahedron, Html } from '../../src'
+import { Icosahedron, Html, OrthographicCamera } from '../../src'
 
 export default {
   title: 'Misc/Html',
@@ -42,3 +42,48 @@ function HTMLScene() {
 
 export const HTMLSt = () => <HTMLScene />
 HTMLSt.storyName = 'Default'
+
+function HTMLOrthographicScene() {
+  const ref = useTurntable()
+
+  const initialCamera = {
+    position: new THREE.Vector3(0, 0, 1500),
+    fov: 40,
+    far: 20000,
+    near: 10,
+    up: new THREE.Vector3(0, 0, 1),
+  }
+
+  return (
+    <>
+      <OrthographicCamera makeDefault={true} applyMatrix4={undefined} {...initialCamera}>
+        <></>
+      </OrthographicCamera>
+      <group ref={ref}>
+        <Icosahedron args={[2, 2]} position={[3, 6, 4]}>
+          <meshBasicMaterial attach="material" color="hotpink" wireframe />
+          <Html scaleFactor={30} className="html-story-block">
+            First
+          </Html>
+        </Icosahedron>
+
+        <Icosahedron args={[2, 2]} position={[10, 0, 10]}>
+          <meshBasicMaterial attach="material" color="hotpink" wireframe />
+          <Html scaleFactor={30} className="html-story-block">
+            Second
+          </Html>
+        </Icosahedron>
+
+        <Icosahedron args={[2, 2]} position={[-10, 0, -10]}>
+          <meshBasicMaterial attach="material" color="hotpink" wireframe />
+          <Html scaleFactor={30} className="html-story-block">
+            Third
+          </Html>
+        </Icosahedron>
+      </group>
+    </>
+  )
+}
+
+export const HTMLOrthoSt = () => <HTMLOrthographicScene />
+HTMLOrthoSt.storyName = 'Orthographic'

c0d572ae499136336ca7bcd743a3263ec2048230
GiuseppeRaso <giusepperaso@aol.it>
2021-02-22 20:49:45 +0100

    avoid duplicate code

--- .storybook/stories/HTML.stories.tsx
+++ .storybook/stories/HTML.stories.tsx
@@ -12,69 +12,58 @@ export default {
   decorators: [(storyFn) => <Setup cameraPosition={new THREE.Vector3(-20, 20, -20)}> {storyFn()}</Setup>],
 }
 
-function HTMLScene() {
+function HTMLScene({
+  transform = false,
+  className = 'html-story-block',
+  color = 'hotpink',
+  children = null,
+}: {
+  transform?: boolean
+  className?: string
+  color?: string
+  children?: React.ReactNode
+}) {
   const ref = useTurntable()
   return (
     <group ref={ref}>
       <Icosahedron args={[2, 2]} position={[3, 6, 4]}>
-        <meshBasicMaterial attach="material" color="hotpink" wireframe />
-        <Html scaleFactor={30} className="html-story-block">
+        <meshBasicMaterial attach="material" color={color} wireframe />
+        <Html transform={transform} scaleFactor={30} className={className}>
           First
         </Html>
       </Icosahedron>
 
       <Icosahedron args={[2, 2]} position={[10, 0, 10]}>
-        <meshBasicMaterial attach="material" color="hotpink" wireframe />
-        <Html scaleFactor={30} className="html-story-block">
+        <meshBasicMaterial attach="material" color={color} wireframe />
+        <Html transform={transform} scaleFactor={30} className={className}>
           Second
         </Html>
       </Icosahedron>
 
       <Icosahedron args={[2, 2]} position={[-10, 0, -10]}>
-        <meshBasicMaterial attach="material" color="hotpink" wireframe />
-        <Html scaleFactor={30} className="html-story-block">
+        <meshBasicMaterial attach="material" color={color} wireframe />
+        <Html transform={transform} scaleFactor={30} className={className}>
           Third
         </Html>
       </Icosahedron>
+      {children}
     </group>
   )
 }
 
 function HTMLTransformScene() {
-  const ref = useTurntable()
   return (
-    <group ref={ref}>
-      <Icosahedron args={[2, 2]} position={[3, 6, 4]}>
-        <meshBasicMaterial attach="material" color="hotpink" wireframe />
-        <Html transform scaleFactor={30} className="html-story-block margin300">
-          First
-        </Html>
-      </Icosahedron>
-
-      <Icosahedron args={[2, 2]} position={[10, 0, 10]}>
-        <meshBasicMaterial attach="material" color="hotpink" wireframe />
-        <Html transform scaleFactor={30} className="html-story-block margin300">
-          Second
-        </Html>
-      </Icosahedron>
-
-      <Icosahedron args={[2, 2]} position={[-10, 0, -10]}>
-        <meshBasicMaterial attach="material" color="hotpink" wireframe />
-        <Html transform scaleFactor={30} className="html-story-block margin300">
-          Third
-        </Html>
-      </Icosahedron>
-
+    <HTMLScene color="palegreen" transform className="html-story-block margin300">
       <Html
         sprite
         transform
         scaleFactor={50}
         position={[5, 15, 0]}
-        style={{ background: 'hotpink', padding: '3px 7px' }}
+        style={{ background: 'palegreen', padding: '3px 7px' }}
       >
         Transform mode
       </Html>
-    </group>
+    </HTMLScene>
   )
 }
 
@@ -82,4 +71,4 @@ export const HTMLSt = () => <HTMLScene />
 HTMLSt.storyName = 'Default'
 
 export const HTMLTransformSt = () => <HTMLTransformScene />
-HTMLTransformSt.storyName = 'Transform flag'
+HTMLTransformSt.storyName = 'Transform mode'

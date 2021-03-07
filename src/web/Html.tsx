pmndrs/drei

commit 17da81fa9d28417704acaee0f5cfc95698bf6fa5

feat: add html3d (#296)
* edited .eslintrc.json to avoid problems with carriage returns

* added description for Html3D

* added and exported Html3D and MixedCanvas

* reverted back eslintrc

* modified setup to allow other types of canvas

* added story for html3d

* modified readme to replace codesandbox with storybook for html3d

* pointermove instead of mousemove

* added type package for resizeobserver

* reverted back Setup

* removed HTML3D story

* reverted back index

* moved the 3d functionalities to Html with tranform flag

* updated README

* updated storybook

* removed resize observer types

* cleaned some old change

* corrections in README

* corrected refs

* updated storybook to reflect ref changes

* added forward ref to element in transform mode

* avoid duplicate code

* html corrections

* added check on refs

* removed void from useLayoutEffect

* improved story for firefox rendering

* removed some useless checks

* restored orto camera and calculate position

Co-authored-by: GiuseppeRaso <giusepperaso@aol.it>

src/web/Html.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Vector3, Group, Object3D, Camera, PerspectiveCamera, OrthographicCamera } from 'three'
+import { Vector3, Group, Object3D, Matrix4, Camera, PerspectiveCamera, OrthographicCamera } from 'three'
 import { Assign } from 'utility-types'
 import { ReactThreeFiber, useFrame, useThree } from 'react-three-fiber'
 
@@ -53,6 +53,25 @@ function objectZIndex(el: Object3D, camera: Camera, zIndexRange: Array<number>) 
   }
   return undefined
 }
+
+const epsilon = (value: number) => (Math.abs(value) < 1e-10 ? 0 : value)
+
+function getCSSMatrix(matrix: Matrix4, multipliers: number[], prepend = '') {
+  let matrix3d = 'matrix3d('
+  for (let i = 0; i !== 16; i++) {
+    matrix3d += epsilon(multipliers[i] * matrix.elements[i]) + (i !== 15 ? ',' : ')')
+  }
+  return prepend + matrix3d
+}
+
+const getCameraCSSMatrix = ((multipliers: number[]) => {
+  return (matrix: Matrix4) => getCSSMatrix(matrix, multipliers)
+})([1, -1, 1, 1, 1, -1, 1, 1, 1, -1, 1, 1, 1, -1, 1, 1])
+
+const getObjectCSSMatrix = ((scaleMultipliers: (n: number) => number[]) => {
+  return (matrix: Matrix4, factor: number) => getCSSMatrix(matrix, scaleMultipliers(factor), 'translate(-50%,-50%)')
+})((f: number) => [1 / f, 1 / f, 1 / f, 1, -1 / f, -1 / f, -1 / f, -1, 1 / f, 1 / f, 1 / f, 1, 1, 1, 1, 1])
+
 export interface HtmlProps
   extends Omit<Assign<React.HTMLAttributes<HTMLDivElement>, ReactThreeFiber.Object3DNode<Group, typeof Group>>, 'ref'> {
   prepend?: boolean
@@ -61,6 +80,8 @@ export interface HtmlProps
   eps?: number
   portal?: React.MutableRefObject<HTMLElement>
   scaleFactor?: number
+  sprite?: boolean
+  transform?: boolean
   zIndexRange?: Array<number>
   calculatePosition?: CalculatePosition
 }
@@ -77,6 +98,8 @@ export const Html = React.forwardRef(
       fullscreen,
       portal,
       scaleFactor,
+      sprite = false,
+      transform = false,
       zIndexRange = [16777271, 0],
       calculatePosition = defaultCalculatePosition,
       ...props
@@ -87,13 +110,19 @@ export const Html = React.forwardRef(
     const [el] = React.useState(() => document.createElement('div'))
     const group = React.useRef<Group>(null)
     const old = React.useRef([0, 0])
-    const target = portal?.current ?? gl.domElement?.parentNode
+    const transformOuterRef = React.useRef<HTMLDivElement>(null)
+    const transformInnerRef = React.useRef<HTMLDivElement>(null)
+    const target = portal?.current ?? gl.domElement.parentNode
 
     React.useEffect(() => {
       if (group.current) {
         scene.updateMatrixWorld()
-        const vec = calculatePosition(group.current, camera, size)
-        el.style.cssText = `position:absolute;top:0;left:0;transform:translate3d(${vec[0]}px,${vec[1]}px,0);transform-origin:0 0;`
+        if (transform) {
+          el.style.cssText = `position:absolute;top:0;left:0;pointer-events:none;overflow:hidden;`
+        } else {
+          const vec = calculatePosition(group.current, camera, size)
+          el.style.cssText = `position:absolute;top:0;left:0;transform:translate3d(${vec[0]}px,${vec[1]}px,0);transform-origin:0 0;`
+        }
         if (target) {
           if (prepend) target.prepend(el)
           else target.appendChild(el)
@@ -104,38 +133,88 @@ export const Html = React.forwardRef(
         }
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [target])
+    }, [target, transform])
 
-    const styles: React.CSSProperties = React.useMemo(
-      () => ({
-        position: 'absolute',
-        transform: center ? 'translate3d(-50%,-50%,0)' : 'none',
-        ...(fullscreen && {
-          top: -size.height / 2,
-          left: -size.width / 2,
+    const styles: React.CSSProperties = React.useMemo(() => {
+      if (transform) {
+        return {
+          position: 'absolute',
+          top: 0,
+          left: 0,
           width: size.width,
           height: size.height,
-        }),
-        ...style,
-      }),
-      [style, center, fullscreen, size]
+          transformStyle: 'preserve-3d',
+          pointerEvents: 'none',
+        }
+      } else {
+        return {
+          position: 'absolute',
+          transform: center ? 'translate3d(-50%,-50%)' : 'none',
+          ...(fullscreen && {
+            top: -size.height / 2,
+            left: -size.width / 2,
+            width: size.width,
+            height: size.height,
+          }),
+          ...style,
+        }
+      }
+    }, [style, center, fullscreen, size, transform])
+
+    const transformInnerStyles: React.CSSProperties = React.useMemo(
+      () => ({ position: 'absolute', pointerEvents: 'auto', ...style }),
+      [style]
     )
 
-    React.useLayoutEffect(
-      () => void ReactDOM.render(<div ref={ref} style={styles} className={className} children={children} />, el)
-    )
+    React.useLayoutEffect(() => {
+      if (transform) {
+        ReactDOM.render(
+          <div ref={transformOuterRef} style={styles}>
+            <div ref={transformInnerRef} style={transformInnerStyles}>
+              <div ref={ref} className={className} children={children} />
+            </div>
+          </div>,
+          el
+        )
+      } else {
+        ReactDOM.render(<div ref={ref} style={styles} className={className} children={children} />, el)
+      }
+    })
 
     useFrame(() => {
       if (group.current) {
         camera.updateMatrixWorld()
-        const vec = calculatePosition(group.current, camera, size)
-        if (Math.abs(old.current[0] - vec[0]) > eps || Math.abs(old.current[1] - vec[1]) > eps) {
+        const vec = transform ? old.current : calculatePosition(group.current, camera, size)
+        if (transform || Math.abs(old.current[0] - vec[0]) > eps || Math.abs(old.current[1] - vec[1]) > eps) {
           el.style.display = !isObjectBehindCamera(group.current, camera) ? 'block' : 'none'
-          const scale = scaleFactor === undefined ? 1 : objectScale(group.current, camera) * scaleFactor
-          el.style.transform = `translate3d(${vec[0]}px,${vec[1]}px,0) scale(${scale})`
           el.style.zIndex = `${objectZIndex(group.current, camera, zIndexRange)}`
+          if (transform) {
+            const [widthHalf, heightHalf] = [size.width / 2, size.height / 2]
+            const fov = camera.projectionMatrix.elements[5] * heightHalf
+            const { isOrthographicCamera, top, left, bottom, right } = camera as OrthographicCamera
+            const cameraMatrix = getCameraCSSMatrix(camera.matrixWorldInverse)
+            const cameraTransform = isOrthographicCamera
+              ? `scale(${fov})translate(${epsilon(-(right + left) / 2)}px,${epsilon((top + bottom) / 2)}px)`
+              : `translateZ(${fov}px)`
+            let matrix = group.current.matrixWorld
+            if (sprite) {
+              matrix = camera.matrixWorldInverse.clone().transpose().copyPosition(matrix).scale(group.current.scale)
+              matrix.elements[3] = matrix.elements[7] = matrix.elements[11] = 0
+              matrix.elements[15] = 1
+            }
+            el.style.width = size.width + 'px'
+            el.style.height = size.height + 'px'
+            el.style.perspective = isOrthographicCamera ? '' : `${fov}px`
+            if (transformOuterRef.current && transformInnerRef.current) {
+              transformOuterRef.current.style.transform = `${cameraTransform}${cameraMatrix}translate(${widthHalf}px,${heightHalf}px)`
+              transformInnerRef.current.style.transform = getObjectCSSMatrix(matrix, 1 / ((scaleFactor || 10) / 400))
+            }
+          } else {
+            const scale = scaleFactor === undefined ? 1 : objectScale(group.current, camera) * scaleFactor
+            el.style.transform = `translate3d(${vec[0]}px,${vec[1]}px,0) scale(${scale})`
+            old.current = vec
+          }
         }
-        old.current = vec
       }
     })
 

.storybook/index.css
@@ -26,6 +26,10 @@ body,
   margin-left: 100px;
 }
 
+.html-story-block.margin300 {
+  margin-left: 300px;
+}
+
 .html-story-block:before {
   content: '';
   display: block;

.storybook/stories/HTML.stories.tsx
@@ -14,30 +14,59 @@ export default {
   decorators: [(storyFn) => <Setup cameraPosition={new THREE.Vector3(-20, 20, -20)}> {storyFn()}</Setup>],
 }
 
-function HTMLScene(htmlProps: HtmlProps) {
+function HTMLScene({
+  children = null,
+  color = 'hotpink',
+  ...htmlProps
+}: HtmlProps & { color?: string; children?: React.ReactNode }) {
   const ref = useTurntable()
   return (
     <group ref={ref}>
       <Icosahedron args={[2, 2]} position={[3, 6, 4]}>
-        <meshBasicMaterial attach="material" color="hotpink" wireframe />
+        <meshBasicMaterial attach="material" color={color} wireframe />
         <Html {...htmlProps}>First</Html>
       </Icosahedron>
 
       <Icosahedron args={[2, 2]} position={[10, 0, 10]}>
-        <meshBasicMaterial attach="material" color="hotpink" wireframe />
+        <meshBasicMaterial attach="material" color={color} wireframe />
         <Html {...htmlProps}>Second</Html>
       </Icosahedron>
 
       <Icosahedron args={[2, 2]} position={[-20, 0, -20]}>
-        <meshBasicMaterial attach="material" color="hotpink" wireframe />
+        <meshBasicMaterial attach="material" color={color} wireframe />
         <Html {...htmlProps}>Third</Html>
       </Icosahedron>
+      {children}
     </group>
   )
 }
 
 export const HTMLSt = () => <HTMLScene scaleFactor={30} className="html-story-block" />
 HTMLSt.storyName = 'Default'
+
+function HTMLTransformScene() {
+  return (
+    <HTMLScene color="palegreen" transform className="html-story-block margin300" scaleFactor={30}>
+      <Html
+        sprite
+        transform
+        scaleFactor={20}
+        position={[5, 15, 0]}
+        style={{
+          background: 'palegreen',
+          fontSize: '50px',
+          padding: '10px 18px',
+          border: '2px solid black',
+        }}
+      >
+        Transform mode
+      </Html>
+    </HTMLScene>
+  )
+}
+
+export const HTMLTransformSt = () => <HTMLTransformScene />
+HTMLTransformSt.storyName = 'Transform mode'
 
 function HTMLOrthographicScene() {
   const ref = useFadeInOut()

commit 7e5b1cef767f6bdfc47c9faa8641b422c4736748

fix: Html center bug introduced in #296 (#322)
Simply a 0 is missing, maybe a copy paste issue. But without the 0 the center property isn't working anymore.

src/web/Html.tsx
@@ -149,7 +149,7 @@ export const Html = React.forwardRef(
       } else {
         return {
           position: 'absolute',
-          transform: center ? 'translate3d(-50%,-50%)' : 'none',
+          transform: center ? 'translate3d(-50%,-50%,0)' : 'none',
           ...(fullscreen && {
             top: -size.height / 2,
             left: -size.width / 2,
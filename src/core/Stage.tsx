80bda2dff78799f43f93f942e3f08ae4bc4c438e
Paul Henschel <drcmda@gmail.com>
2021-04-13 13:55:15 +0200

    fix: add presets to stage, set defaults

--- src/core/Stage.tsx
+++ src/core/Stage.tsx
@@ -5,25 +5,49 @@ import { Environment } from './Environment'
 import { ContactShadows } from './ContactShadows'
 import { PresetsType } from '../helpers/environment-assets'
 
+const presets = {
+  rembrandt: {
+    main: [1, 2, 1],
+    fill: [-2, -0.5, -2],
+  },
+  portrait: {
+    main: [-1, 2, 0.5],
+    fill: [-1, 0.5, -1.5],
+  },
+  upfront: {
+    main: [0, 2, 1],
+    fill: [-1, 0.5, -1.5],
+  },
+  soft: {
+    main: [-2, 4, 4],
+    fill: [-1, 0.5, -1.5],
+  },
+}
+
 type Props = JSX.IntrinsicElements['group'] & {
   contactShadow?: boolean
   shadows?: boolean
   adjustCamera?: boolean
   environment?: PresetsType
   intensity?: number
+  ambience?: number
   controls?: React.MutableRefObject<{ update(): void; target: THREE.Vector3 }>
+  preset?: keyof typeof presets
 }
 
 export function Stage({
   children,
   controls,
-  contactShadow,
-  shadows,
-  adjustCamera,
-  environment,
+  shadows = true,
+  adjustCamera = true,
+  environment = 'city',
+  contactShadow = true,
   intensity = 1,
+  ambience = 0.3,
+  preset = 'rembrandt',
   ...props
 }: Props) {
+  const config = presets[preset]
   const camera = useThree((state) => state.camera)
   const outer = React.useRef<THREE.Group>(null!)
   const inner = React.useRef<THREE.Group>(null!)
@@ -46,7 +70,7 @@ export function Stage({
   React.useLayoutEffect(() => {
     if (adjustCamera) {
       const y = radius / (height > width ? 1.5 : 2.5)
-      camera.position.set(0, radius * 1.5, radius * 2.5)
+      camera.position.set(0, radius * 0.5, radius * 2.5)
       camera.near = 0.1
       camera.far = Math.max(5000, radius * 4)
       camera.lookAt(0, y, 0)
@@ -72,8 +96,16 @@ export function Stage({
           far={radius / 2}
         />
       )}
-      <spotLight position={[radius, radius * 2, radius]} intensity={intensity * 2} castShadow={shadows} />
-      <pointLight position={[-radius * 2, -radius * 2, -radius * 2]} intensity={intensity} />
+      <ambientLight intensity={ambience} />
+      <spotLight
+        position={[config.main[0] * radius, config.main[1] * radius, config.main[2] * radius]}
+        intensity={intensity * 2}
+        castShadow={shadows}
+      />
+      <pointLight
+        position={[config.fill[0] * radius, config.fill[1] * radius, config.fill[2] * radius]}
+        intensity={intensity}
+      />
       {environment && <Environment preset={environment} />}
     </group>
   )

6a50e6d02f66e991cf2854186d1ef2893861185f
m9dfukc <andreas@invertednothing.com>
2021-04-22 11:16:15 +0200

    updated troika-three-text dependency for bidi and ltr support

--- .storybook/stories/Text.stories.tsx
+++ .storybook/stories/Text.stories.tsx
@@ -107,16 +107,32 @@ function TextShadowScene() {
       font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
       anchorX="center"
       anchorY="middle"
-      outlineOffsetX={'10%'}
-      outlineOffsetY={'10%'}
-      outlineBlur={'30%'}
-      outlineOpacity={0.3}
-      outlineColor="#EC2D2D"
     >
       LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
       MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
       CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
       EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.
+    </Text>
+  )
+}
+function TextLtrScene() {
+  const ref = useTurntable()
+
+  return (
+    <Text
+      ref={ref}
+      color={'#EC2D2D'}
+      fontSize={12}
+      maxWidth={200}
+      lineHeight={1}
+      letterSpacing={0.02}
+      textAlign={'right'}
+      direction={'auto'}
+      font="https://fonts.gstatic.com/s/scheherazade/v20/YA9Ur0yF4ETZN60keViq1kQgtA.woff"
+      anchorX="center"
+      anchorY="middle"
+    >
+      ان عدة الشهور عند الله اثنا عشر شهرا في كتاب الله يوم خلق السماوات والارض SOME LATIN TEXT HERE منها اربعة حرم ذلك الدين القيم فلاتظلموا فيهن انفسكم وقاتلوا المشركين كافة كما يقاتلونكم كافة واعلموا ان الله مع المتقين
     </Text>
   )
 }
@@ -159,6 +175,9 @@ TextStrokeSt.storyName = 'Transparent with stroke'
 
 export const TextShadowSt = () => <TextShadowScene />
 TextShadowSt.storyName = 'Text Shadow'
+
+export const TextLtrSt = () => <TextLtrScene />
+TextLtrSt.storyName = 'Text Ltr'
 
 export const CustomMaterialTextSt = () => <CustomMaterialTextScene />
 CustomMaterialTextSt.storyName = 'Custom Material'

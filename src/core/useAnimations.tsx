a29778ae995f2f5eeff7a12abfb47dd08cd046d2
Denis Zimmermann <denis.zimmermann@comwatt.com>
2021-04-26 11:25:36 +0200

    fix: useAnimations was not assigning the actions
    see the issue for details
    
    closes issue #377

--- src/core/useAnimations.tsx
+++ src/core/useAnimations.tsx
@@ -2,38 +2,46 @@ import * as React from 'react'
 import { Object3D, AnimationClip, AnimationAction, AnimationMixer } from 'three'
 import { useFrame } from '@react-three/fiber'
 
-type Api<T> = {
+type Api<T extends AnimationClip> = {
   ref: React.MutableRefObject<Object3D | undefined | null>
   clips: AnimationClip[]
   mixer: AnimationMixer
-  names: string[]
-  actions: T
+  names: T['name'][]
+  actions: { [key in T['name']]: AnimationAction | null }
 }
 
-export function useAnimations<
-  T = {
-    [key: string]: AnimationAction
-  }
->(clips: AnimationClip[], root?: React.MutableRefObject<Object3D | undefined | null>) {
+export function useAnimations<T extends AnimationClip>(
+  clips: T[],
+  root?: React.MutableRefObject<Object3D | undefined | null>
+): Api<T> {
   const ref = React.useRef<Object3D>()
   const actualRef = root ? root : ref
   const [mixer] = React.useState(() => new AnimationMixer((undefined as unknown) as Object3D))
-  const api = React.useMemo(
-    () => ({
-      ref: actualRef,
-      clips,
-      actions: clips.reduce((prev, clip) => ({ ...prev, [clip.name]: null }), {}),
-      names: clips.map((clip) => clip.name),
-      mixer,
-    }),
-    [clips, mixer, actualRef]
-  )
+  const [api, setApi] = React.useState<Api<T>>({
+    ref: actualRef,
+    clips,
+    actions: clips.reduce((prev, clip) => ({ ...prev, [clip.name]: null }), {} as Api<T>['actions']),
+    names: clips.map((clip) => clip.name),
+    mixer,
+  })
   useFrame((state, delta) => mixer.update(delta))
   React.useEffect(() => {
     const currentRoot = actualRef.current
     if (currentRoot) {
-      clips.forEach((clip) => (api.actions[clip.name] = mixer.clipAction(clip, currentRoot)))
+      setApi((oldApi) => ({
+        ...oldApi,
+        actions: clips.reduce(
+          (prev, clip) => ({
+            ...prev,
+            [clip.name]: mixer.clipAction(clip, currentRoot),
+          }),
+          oldApi.actions
+        ),
+      }))
     }
+  }, [clips, mixer, root, actualRef])
+  React.useEffect(() => {
+    const currentRoot = actualRef.current
     return () =>
       Object.values(api.actions).forEach((action) => {
         if (currentRoot) {
@@ -41,5 +49,5 @@ export function useAnimations<
         }
       })
   }, [api, clips, mixer, root, actualRef])
-  return api as Api<T>
+  return api
 }

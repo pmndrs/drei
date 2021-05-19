3d68029996775799a23826d3e036e139656d5fc2
Paul Henschel <drcmda@gmail.com>
2021-05-19 16:09:57 +0200

    fix: typo in useTexture

--- src/core/useTexture.tsx
+++ src/core/useTexture.tsx
@@ -7,13 +7,13 @@ export const IsObject = (url: any): url is Record<string, string> =>
 export function useTexture<Url extends string[] | string | Record<string, string>>(
   input: Url
 ): Url extends any[] ? Texture[] : Url extends object ? { [key in keyof Url]: Texture } : Texture {
+  const textures = useLoader(TextureLoader, IsObject(input) ? Object.values(input) : input as any)
   if (IsObject(input)) {
-    const [keys, values] = Object.entries(input)
-    const textures = useTexture(values)
+    const keys = Object.keys(input)
     const keyed = {} as any
     keys.forEach((key) => Object.assign(keyed, { [key]: textures[keys.indexOf(key)] }))
     return keyed
-  } else return useLoader(TextureLoader, input as any) as any
+  } else return textures as any
 }
 
 useTexture.preload = (url: string extends any[] ? string[] : string) => useLoader.preload(TextureLoader, url)

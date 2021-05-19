77e0480f24504faabe8417ff705d627e8e28d931
Paul Henschel <drcmda@gmail.com>
2021-05-19 15:55:22 +0200

    fix: useTexture accepts key:url objects

--- src/core/useTexture.tsx
+++ src/core/useTexture.tsx
@@ -1,8 +1,19 @@
 import { Texture, TextureLoader } from 'three'
 import { useLoader } from '@react-three/fiber'
 
-export function useTexture<Url extends string[] | string>(url: Url): Url extends any[] ? Texture[] : Texture {
-  return useLoader(TextureLoader, url)
+export const IsObject = (url: any): url is Record<string, string> =>
+  url === Object(url) && !Array.isArray(url) && typeof url !== 'function'
+
+export function useTexture<Url extends string[] | string | Record<string, string>>(
+  input: Url
+): Url extends any[] ? Texture[] : Url extends object ? { [key in keyof Url]: Texture } : Texture {
+  if (IsObject(input)) {
+    const [keys, values] = Object.entries(input)
+    const textures = useTexture(values)
+    const keyed = {} as any
+    keys.forEach((key) => Object.assign(keyed, { [key]: textures[keys.indexOf(key)] }))
+    return keyed
+  } else return useLoader(TextureLoader, input as any) as any
 }
 
 useTexture.preload = (url: string extends any[] ? string[] : string) => useLoader.preload(TextureLoader, url)

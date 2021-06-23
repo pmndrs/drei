9b615c465037b620bbc0b62049577f188a2439de
Paul Henschel <drcmda@gmail.com>
2021-06-23 11:50:23 +0200

    fix return

--- src/core/useDetectGPU.tsx
+++ src/core/useDetectGPU.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { getGPUTier, GetGPUTier, TierResult } from 'detect-gpu'
 import { useAsset } from 'use-asset'
 
-export function useDetectGPU(props?: GetGPUTier): TierResult | null {
+export function useDetectGPU(props?: GetGPUTier) {
   const [GPUTier, setGPUTier] = React.useState<TierResult | null>(null)
   return useAsset<TierResult, [string]>(() => getGPUTier(props), 'useDetectGPU')
 }

d7dea9f97feec3b4a1e9f9acbe1ed27e044f02a3
Paul Henschel <drcmda@gmail.com>
2021-07-13 10:10:57 +0200

    feat: add gradient texture

--- src/core/GradientTexture.tsx
+++ src/core/GradientTexture.tsx
@@ -10,18 +10,18 @@ type Props = {
 
 export function GradientTexture({ stops, colors, size = 2048, ...props }: Props) {
   const texture = React.useMemo(() => {
-    let canvas = document.createElement('canvas')
-    let context = canvas.getContext('2d')!
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')!
     canvas.width = 16
     canvas.height = size
-    let gradient = context.createLinearGradient(0, 0, 0, size)
+    const gradient = context.createLinearGradient(0, 0, 0, size)
     let i = stops.length
     while (i--) {
       gradient.addColorStop(stops[i], colors[i])
     }
     context.fillStyle = gradient
     context.fillRect(0, 0, 16, size)
-    let texture = new THREE.Texture(canvas)
+    const texture = new THREE.Texture(canvas)
     texture.needsUpdate = true
     return texture
   }, [stops])

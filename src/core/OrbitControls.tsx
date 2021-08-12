4cab61d3a737292dff4f9c9cfcc996582a774d60
Lewy Blue <lewy@discoverthreejs.com>
2021-08-12 11:07:49 +0700

    feat: add onStart, onEnd, onChange props to OrbitControls

--- src/core/OrbitControls.tsx
+++ src/core/OrbitControls.tsx
@@ -1,5 +1,6 @@
+import { ReactThreeFiber, useFrame, useThree } from '@react-three/fiber'
 import * as React from 'react'
-import { ReactThreeFiber, useThree, useFrame } from '@react-three/fiber'
+import { Event } from 'three'
 import { OrbitControls as OrbitControlsImpl } from 'three-stdlib'
 
 export type OrbitControlsProps = ReactThreeFiber.Overwrite<
@@ -11,11 +12,14 @@ export type OrbitControlsProps = ReactThreeFiber.Overwrite<
     regress?: boolean
     enableDamping?: boolean
     makeDefault?: boolean
+    onChange?: (e?: Event) => void
+    onStart?: (e?: Event) => void
+    onEnd?: (e?: Event) => void
   }
 >
 
 export const OrbitControls = React.forwardRef<OrbitControlsImpl, OrbitControlsProps>(
-  ({ makeDefault, camera, regress, domElement, enableDamping = true, ...restProps }, ref) => {
+  ({ makeDefault, camera, regress, domElement, enableDamping = true, onChange, onStart, onEnd, ...restProps }, ref) => {
     const invalidate = useThree(({ invalidate }) => invalidate)
     const defaultCamera = useThree(({ camera }) => camera)
     const gl = useThree(({ gl }) => gl)
@@ -31,15 +35,22 @@ export const OrbitControls = React.forwardRef<OrbitControlsImpl, OrbitControlsPr
     })
 
     React.useEffect(() => {
-      const callback = () => {
+      const callback = (e) => {
         invalidate()
         if (regress) performance.regress()
+        if (onChange) onChange(e)
       }
 
       controls.connect(explDomElement)
       controls.addEventListener('change', callback)
+
+      if (onStart) controls.addEventListener('start', onStart)
+      if (onEnd) controls.addEventListener('end', onEnd)
+
       return () => {
         controls.removeEventListener('change', callback)
+        if (onStart) controls.removeEventListener('start', onStart)
+        if (onEnd) controls.removeEventListener('end', onEnd)
         controls.dispose()
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps

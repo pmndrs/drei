8a6805aec0e702282abcab0bfe8af0712cc7c921
Paul Henschel <drcmda@gmail.com>
2021-09-18 00:32:18 +0200

    fix: allow refs on points/instances

--- src/core/Instances.tsx
+++ src/core/Instances.tsx
@@ -28,79 +28,87 @@ const translation = new THREE.Vector3()
 const rotation = new THREE.Quaternion()
 const scale = new THREE.Vector3()
 
-function Instances({ children, range, limit = 1000, ...props }: InstancesProps) {
-  const parentRef = React.useRef<InstancedMesh>(null!)
-  const [instances, setInstances] = React.useState<React.MutableRefObject<Position>[]>([])
-  const [[matrices, colors]] = React.useState(() => {
-    const mArray = new Float32Array(limit * 16)
-    for (i = 0; i < limit; i++) tempMatrix.identity().toArray(mArray, i * 16)
-    return [mArray, new Float32Array([...new Array(limit * 3)].map(() => 1))]
-  })
+const Instances = React.forwardRef(
+  ({ children, range, limit = 1000, ...props }: InstancesProps, ref: React.ForwardedRef<THREE.InstancedMesh>) => {
+    const parentRef = React.useRef<InstancedMesh>(null!)
+    const [instances, setInstances] = React.useState<React.MutableRefObject<Position>[]>([])
+    const [[matrices, colors]] = React.useState(() => {
+      const mArray = new Float32Array(limit * 16)
+      for (i = 0; i < limit; i++) tempMatrix.identity().toArray(mArray, i * 16)
+      return [mArray, new Float32Array([...new Array(limit * 3)].map(() => 1))]
+    })
 
-  React.useLayoutEffect(() => {
-    parentRef.current.count =
-      parentRef.current.instanceMatrix.updateRange.count =
-      parentRef.current.instanceColor.updateRange.count =
-        Math.min(limit, range !== undefined ? range : limit, instances.length)
-  }, [instances, range])
+    React.useLayoutEffect(() => {
+      parentRef.current.count =
+        parentRef.current.instanceMatrix.updateRange.count =
+        parentRef.current.instanceColor.updateRange.count =
+          Math.min(limit, range !== undefined ? range : limit, instances.length)
+    }, [instances, range])
 
-  React.useEffect(() => {
-    // We might be a frame too late? 🤷‍♂️
-    parentRef.current.instanceMatrix.needsUpdate = true
-  })
+    React.useEffect(() => {
+      // We might be a frame too late? 🤷‍♂️
+      parentRef.current.instanceMatrix.needsUpdate = true
+    })
 
-  useFrame((state) => {
-    parentRef.current.updateMatrix()
-    parentRef.current.updateMatrixWorld()
-    parentMatrix.copy(parentRef.current.matrixWorld).invert()
-    for (i = 0; i < instances.length; i++) {
-      instanceRef = instances[i].current
-      // Multiply the inverse of the InstancedMesh world matrix or else
-      // Instances will be double-transformed if <Instances> isn't at identity
-      instanceRef.matrixWorld.decompose(translation, rotation, scale)
-      instanceMatrix.compose(translation, rotation, scale).premultiply(parentMatrix)
-      if (!instanceMatrix.equals(tempMatrix.fromArray(matrices, i * 16))) {
-        instanceMatrix.toArray(matrices, i * 16)
-        parentRef.current.instanceMatrix.needsUpdate = true
+    useFrame((state) => {
+      parentRef.current.updateMatrix()
+      parentRef.current.updateMatrixWorld()
+      parentMatrix.copy(parentRef.current.matrixWorld).invert()
+      for (i = 0; i < instances.length; i++) {
+        instanceRef = instances[i].current
+        // Multiply the inverse of the InstancedMesh world matrix or else
+        // Instances will be double-transformed if <Instances> isn't at identity
+        instanceRef.matrixWorld.decompose(translation, rotation, scale)
+        instanceMatrix.compose(translation, rotation, scale).premultiply(parentMatrix)
+        if (!instanceMatrix.equals(tempMatrix.fromArray(matrices, i * 16))) {
+          instanceMatrix.toArray(matrices, i * 16)
+          parentRef.current.instanceMatrix.needsUpdate = true
+        }
+
+        if (!instanceRef.color.equals(color.fromArray(colors, i * 3))) {
+          instanceRef.color.toArray(colors, i * 3)
+          parentRef.current.instanceColor.needsUpdate = true
+        }
       }
+    })
 
-      if (!instanceRef.color.equals(color.fromArray(colors, i * 3))) {
-        instanceRef.color.toArray(colors, i * 3)
-        parentRef.current.instanceColor.needsUpdate = true
-      }
-    }
-  })
+    const events = React.useMemo(() => {
+      const events = {}
+      for (i = 0; i < instances.length; i++) Object.assign(events, (instances[i].current as any)?.__r3f.handlers)
+      return Object.keys(events).reduce(
+        (prev, key) => ({
+          ...prev,
+          [key]: (e) => (instances[e.instanceId].current as any)?.__r3f?.handlers?.[key](e),
+        }),
+        {}
+      )
+    }, [instances])
 
-  const events = React.useMemo(() => {
-    const events = {}
-    for (i = 0; i < instances.length; i++) Object.assign(events, (instances[i].current as any)?.__r3f.handlers)
-    return Object.keys(events).reduce(
-      (prev, key) => ({
-        ...prev,
-        [key]: (e) => (instances[e.instanceId].current as any)?.__r3f?.handlers?.[key](e),
+    const api = React.useMemo(
+      () => ({
+        subscribe: (ref) => {
+          setInstances((instances) => [...instances, ref])
+          return () => setInstances((instances) => instances.filter((item) => item.current !== ref.current))
+        },
       }),
-      {}
+      []
     )
-  }, [instances])
 
-  const api = React.useMemo(
-    () => ({
-      subscribe: (ref) => {
-        setInstances((instances) => [...instances, ref])
-        return () => setInstances((instances) => instances.filter((item) => item.current !== ref.current))
-      },
-    }),
-    []
-  )
-
-  return (
-    <instancedMesh matrixAutoUpdate={false} ref={parentRef} args={[null as any, null as any, 0]} {...events} {...props}>
-      <instancedBufferAttribute attach="instanceMatrix" count={matrices.length / 16} array={matrices} itemSize={16} />
-      <instancedBufferAttribute attach="instanceColor" count={colors.length / 3} array={colors} itemSize={3} />
-      <context.Provider value={api}>{children}</context.Provider>
-    </instancedMesh>
-  )
-}
+    return (
+      <instancedMesh
+        matrixAutoUpdate={false}
+        ref={mergeRefs([ref, parentRef])}
+        args={[null as any, null as any, 0]}
+        {...events}
+        {...props}
+      >
+        <instancedBufferAttribute attach="instanceMatrix" count={matrices.length / 16} array={matrices} itemSize={16} />
+        <instancedBufferAttribute attach="instanceColor" count={colors.length / 3} array={colors} itemSize={3} />
+        <context.Provider value={api}>{children}</context.Provider>
+      </instancedMesh>
+    )
+  }
+)
 
 const Instance = React.forwardRef(({ children, ...props }, ref) => {
   React.useMemo(() => extend({ Position }), [])
--- src/core/Points.tsx
+++ src/core/Points.tsx
@@ -19,80 +19,87 @@ const parentMatrix = new THREE.Matrix4()
 const position = new THREE.Vector3()
 const color = new THREE.Color()
 
-function Points({ children, range, limit = 1000, ...props }: PointsProps) {
-  const parentRef = React.useRef<THREE.Points>(null!)
-  const [refs, setRefs] = React.useState<React.MutableRefObject<Position>[]>([])
-  const [[positions, colors]] = React.useState(() => [
-    new Float32Array(limit * 3),
-    new Float32Array([...new Array(limit * 3)].map(() => 1)),
-  ])
+const Points = React.forwardRef(
+  ({ children, range, limit = 1000, ...props }: PointsProps, ref: React.ForwardedRef<THREE.Points>) => {
+    const parentRef = React.useRef<THREE.Points>(null!)
+    const [refs, setRefs] = React.useState<React.MutableRefObject<Position>[]>([])
+    const [[positions, colors]] = React.useState(() => [
+      new Float32Array(limit * 3),
+      new Float32Array([...new Array(limit * 3)].map(() => 1)),
+    ])
 
-  React.useLayoutEffect(() => {
-    parentRef.current.geometry.drawRange.count = Math.min(limit, range !== undefined ? range : limit, refs.length)
-  }, [refs, range])
+    React.useLayoutEffect(() => {
+      parentRef.current.geometry.drawRange.count = Math.min(limit, range !== undefined ? range : limit, refs.length)
+    }, [refs, range])
 
-  React.useEffect(() => {
-    // We might be a frame too late? 🤷‍♂️
-    parentRef.current.geometry.attributes.position.needsUpdate = true
-  })
+    React.useEffect(() => {
+      // We might be a frame too late? 🤷‍♂️
+      parentRef.current.geometry.attributes.position.needsUpdate = true
+    })
 
-  useFrame(() => {
-    parentRef.current.updateMatrix()
-    parentRef.current.updateMatrixWorld()
-    parentMatrix.copy(parentRef.current.matrixWorld).invert()
-    for (i = 0; i < refs.length; i++) {
-      positionRef = refs[i].current
-      positionRef.getWorldPosition(position).applyMatrix4(parentMatrix)
-      if (
-        position.x !== positions[i * 3] ||
-        position.y !== positions[i * 3 + 1] ||
-        position.z !== positions[i * 3 + 2]
-      ) {
-        position.toArray(positions, i * 3)
-        parentRef.current.geometry.attributes.position.needsUpdate = true
-        positionRef.matrixWorldNeedsUpdate = true
+    useFrame(() => {
+      parentRef.current.updateMatrix()
+      parentRef.current.updateMatrixWorld()
+      parentMatrix.copy(parentRef.current.matrixWorld).invert()
+      for (i = 0; i < refs.length; i++) {
+        positionRef = refs[i].current
+        positionRef.getWorldPosition(position).applyMatrix4(parentMatrix)
+        if (
+          position.x !== positions[i * 3] ||
+          position.y !== positions[i * 3 + 1] ||
+          position.z !== positions[i * 3 + 2]
+        ) {
+          position.toArray(positions, i * 3)
+          parentRef.current.geometry.attributes.position.needsUpdate = true
+          positionRef.matrixWorldNeedsUpdate = true
+        }
+        if (!positionRef.color.equals(color.fromArray(colors, i * 3))) {
+          positionRef.color.toArray(colors, i * 3)
+          parentRef.current.geometry.attributes.color.needsUpdate = true
+        }
       }
-      if (!positionRef.color.equals(color.fromArray(colors, i * 3))) {
-        positionRef.color.toArray(colors, i * 3)
-        parentRef.current.geometry.attributes.color.needsUpdate = true
-      }
-    }
-  })
+    })
 
-  const events = React.useMemo(() => {
-    const events = {}
-    for (i = 0; i < refs.length; i++) Object.assign(events, (refs[i].current as any)?.__r3f.handlers)
-    return Object.keys(events).reduce(
-      (prev, key) => ({ ...prev, [key]: (e) => (refs[e.index].current as any)?.__r3f?.handlers?.[key](e) }),
-      {}
+    const events = React.useMemo(() => {
+      const events = {}
+      for (i = 0; i < refs.length; i++) Object.assign(events, (refs[i].current as any)?.__r3f.handlers)
+      return Object.keys(events).reduce(
+        (prev, key) => ({ ...prev, [key]: (e) => (refs[e.index].current as any)?.__r3f?.handlers?.[key](e) }),
+        {}
+      )
+    }, [refs])
+
+    const api: Api = React.useMemo(
+      () => ({
+        subscribe: (ref) => {
+          setRefs((refs) => [...refs, ref])
+          return () => setRefs((refs) => refs.filter((item) => item.current !== ref.current))
+        },
+      }),
+      []
     )
-  }, [refs])
 
-  const api: Api = React.useMemo(
-    () => ({
-      subscribe: (ref) => {
-        setRefs((refs) => [...refs, ref])
-        return () => setRefs((refs) => refs.filter((item) => item.current !== ref.current))
-      },
-    }),
-    []
-  )
-
-  return (
-    <points matrixAutoUpdate={false} ref={parentRef} {...events} {...props}>
-      <bufferGeometry>
-        <bufferAttribute
-          attachObject={['attributes', 'position']}
-          count={positions.length / 3}
-          array={positions}
-          itemSize={3}
-        />
-        <bufferAttribute attachObject={['attributes', 'color']} count={colors.length / 3} array={colors} itemSize={3} />
-      </bufferGeometry>
-      <context.Provider value={api}>{children}</context.Provider>
-    </points>
-  )
-}
+    return (
+      <points matrixAutoUpdate={false} ref={mergeRefs([ref, parentRef])} {...events} {...props}>
+        <bufferGeometry>
+          <bufferAttribute
+            attachObject={['attributes', 'position']}
+            count={positions.length / 3}
+            array={positions}
+            itemSize={3}
+          />
+          <bufferAttribute
+            attachObject={['attributes', 'color']}
+            count={colors.length / 3}
+            array={colors}
+            itemSize={3}
+          />
+        </bufferGeometry>
+        <context.Provider value={api}>{children}</context.Provider>
+      </points>
+    )
+  }
+)
 
 const Point = React.forwardRef(({ children, ...props }, ref) => {
   React.useMemo(() => extend({ Position }), [])

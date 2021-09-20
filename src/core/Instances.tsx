5f52513d034d712d4b85360fc5a902fffeeff393
Paul Henschel <drcmda@gmail.com>
2021-09-20 15:13:16 +0200

    feat: merged

--- src/core/Instances.tsx
+++ src/core/Instances.tsx
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import * as React from 'react'
 import { extend, useFrame } from '@react-three/fiber'
 import mergeRefs from 'react-merge-refs'
+import Composer from 'react-composer'
 import { Position } from '../helpers/Position'
 
 type Api = {
@@ -11,6 +12,11 @@ type Api = {
 type InstancesProps = JSX.IntrinsicElements['instancedMesh'] & {
   range?: number
   limit?: number
+  frames?: number
+}
+
+type InstanceProps = JSX.IntrinsicElements['position'] & {
+  context?: React.Context<Api>
 }
 
 type InstancedMesh = Omit<THREE.InstancedMesh, 'instanceMatrix' | 'instanceColor'> & {
@@ -19,7 +25,7 @@ type InstancedMesh = Omit<THREE.InstancedMesh, 'instanceMatrix' | 'instanceColor
 }
 
 let i, instanceRef
-const context = React.createContext<Api>(null!)
+const globalContext = React.createContext<Api>(null!)
 const parentMatrix = new THREE.Matrix4()
 const instanceMatrix = new THREE.Matrix4()
 const tempMatrix = new THREE.Matrix4()
@@ -28,8 +34,29 @@ const translation = new THREE.Vector3()
 const rotation = new THREE.Quaternion()
 const scale = new THREE.Vector3()
 
+const Instance = React.forwardRef(({ context, children, ...props }: InstanceProps, ref) => {
+  React.useMemo(() => extend({ Position }), [])
+  const group = React.useRef()
+  const { subscribe } = React.useContext(context || globalContext)
+  React.useLayoutEffect(() => subscribe(group), [])
+  return (
+    <position ref={mergeRefs([ref, group])} {...props}>
+      {children}
+    </position>
+  )
+})
+
 const Instances = React.forwardRef(
-  ({ children, range, limit = 1000, ...props }: InstancesProps, ref: React.ForwardedRef<THREE.InstancedMesh>) => {
+  (
+    { children, range, limit = 1000, frames = Infinity, ...props }: InstancesProps,
+    ref: React.ForwardedRef<THREE.InstancedMesh>
+  ) => {
+    const [[context, instance]] = React.useState<[React.Context<Api>, (props: InstanceProps) => JSX.Element]>(() => {
+      const c = React.createContext<Api>(null!)
+      const i = (props: InstanceProps) => <Instance context={c} {...props} />
+      return [c, i]
+    })
+
     const parentRef = React.useRef<InstancedMesh>(null!)
     const [instances, setInstances] = React.useState<React.MutableRefObject<Position>[]>([])
     const [[matrices, colors]] = React.useState(() => {
@@ -50,25 +77,29 @@ const Instances = React.forwardRef(
       parentRef.current.instanceMatrix.needsUpdate = true
     })
 
+    let count = 0
     useFrame((state) => {
-      parentRef.current.updateMatrix()
-      parentRef.current.updateMatrixWorld()
-      parentMatrix.copy(parentRef.current.matrixWorld).invert()
-      for (i = 0; i < instances.length; i++) {
-        instanceRef = instances[i].current
-        // Multiply the inverse of the InstancedMesh world matrix or else
-        // Instances will be double-transformed if <Instances> isn't at identity
-        instanceRef.matrixWorld.decompose(translation, rotation, scale)
-        instanceMatrix.compose(translation, rotation, scale).premultiply(parentMatrix)
-        if (!instanceMatrix.equals(tempMatrix.fromArray(matrices, i * 16))) {
-          instanceMatrix.toArray(matrices, i * 16)
-          parentRef.current.instanceMatrix.needsUpdate = true
-        }
+      if (frames === Infinity || count < frames) {
+        parentRef.current.updateMatrix()
+        parentRef.current.updateMatrixWorld()
+        parentMatrix.copy(parentRef.current.matrixWorld).invert()
+        for (i = 0; i < instances.length; i++) {
+          instanceRef = instances[i].current
+          // Multiply the inverse of the InstancedMesh world matrix or else
+          // Instances will be double-transformed if <Instances> isn't at identity
+          instanceRef.matrixWorld.decompose(translation, rotation, scale)
+          instanceMatrix.compose(translation, rotation, scale).premultiply(parentMatrix)
+          if (!instanceMatrix.equals(tempMatrix.fromArray(matrices, i * 16))) {
+            instanceMatrix.toArray(matrices, i * 16)
+            parentRef.current.instanceMatrix.needsUpdate = true
+          }
 
-        if (!instanceRef.color.equals(color.fromArray(colors, i * 3))) {
-          instanceRef.color.toArray(colors, i * 3)
-          parentRef.current.instanceColor.needsUpdate = true
+          if (!instanceRef.color.equals(color.fromArray(colors, i * 3))) {
+            instanceRef.color.toArray(colors, i * 3)
+            parentRef.current.instanceColor.needsUpdate = true
+          }
         }
+        count++
       }
     })
 
@@ -102,24 +133,47 @@ const Instances = React.forwardRef(
         {...events}
         {...props}
       >
-        <instancedBufferAttribute attach="instanceMatrix" count={matrices.length / 16} array={matrices} itemSize={16} />
-        <instancedBufferAttribute attach="instanceColor" count={colors.length / 3} array={colors} itemSize={3} />
-        <context.Provider value={api}>{children}</context.Provider>
+        <instancedBufferAttribute
+          attach="instanceMatrix"
+          count={matrices.length / 16}
+          array={matrices}
+          itemSize={16}
+          usage={THREE.DynamicDrawUsage}
+          needsUpdate={true}
+        />
+        <instancedBufferAttribute
+          attach="instanceColor"
+          count={colors.length / 3}
+          array={colors}
+          itemSize={3}
+          usage={THREE.DynamicDrawUsage}
+          needsUpdate={true}
+        />
+        {typeof children === 'function' ? (
+          <context.Provider value={api}>{children(instance)}</context.Provider>
+        ) : (
+          <globalContext.Provider value={api}>{children}</globalContext.Provider>
+        )}
       </instancedMesh>
     )
   }
 )
 
-const Instance = React.forwardRef(({ children, ...props }, ref) => {
-  React.useMemo(() => extend({ Position }), [])
-  const group = React.useRef()
-  const { subscribe } = React.useContext(context)
-  React.useLayoutEffect(() => subscribe(group), [])
+function Merged({ meshes, children, ...props }) {
+  const isArray = Array.isArray(meshes)
   return (
-    <position ref={mergeRefs([ref, group])} {...props}>
-      {children}
-    </position>
+    <Composer
+      components={(isArray ? meshes : Object.values(meshes)).map(({ geometry, material }) => (
+        <Instances geometry={geometry} material={material} {...props} />
+      ))}
+    >
+      {(args) =>
+        isArray
+          ? children(...args)
+          : children(Object.keys(meshes).reduce((acc, key, i) => ({ ...acc, [key]: args[i] }), {}))
+      }
+    </Composer>
   )
-})
+}
 
-export { Instances, Instance }
+export { Instances, Instance, Merged }

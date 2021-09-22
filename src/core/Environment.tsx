e42f9d2136dfbf4efa34c5c0e33e121b4e4a2c1e
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-09-22 17:31:38 -0500

    chore(Environment): remove PMREMGenerator

--- src/core/Environment.tsx
+++ src/core/Environment.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react'
 import { useLoader, useThree } from '@react-three/fiber'
-import { CubeTexture, CubeTextureLoader, Texture, PMREMGenerator, Scene, Loader } from 'three'
+import {
+  CubeTextureLoader,
+  Texture,
+  CubeRefractionMapping,
+  EquirectangularReflectionMapping,
+  Scene,
+  Loader,
+  FloatType,
+} from 'three'
 import { RGBELoader } from 'three-stdlib'
-import { useAsset } from 'use-asset'
-
 import { presetsObj, PresetsType } from '../helpers/environment-assets'
-
-function getTexture(texture: Texture | CubeTexture, gen: PMREMGenerator, isCubeMap: boolean) {
-  if (isCubeMap) {
-    gen.compileEquirectangularShader()
-    return gen.fromCubemap(texture as CubeTexture).texture
-  }
-  return gen.fromEquirectangular(texture).texture
-}
 
 const CUBEMAP_ROOT = 'https://rawcdn.githack.com/pmndrs/drei-assets/aa3600359ba664d546d05821bcbca42013587df2'
 
@@ -41,28 +39,16 @@ export function Environment({
     path = CUBEMAP_ROOT + '/hdri/'
   }
   const defaultScene = useThree(({ scene }) => scene)
-  const gl = useThree(({ gl }) => gl)
   const isCubeMap = Array.isArray(files)
   const loader = isCubeMap ? CubeTextureLoader : RGBELoader
   // @ts-expect-error
   const loaderResult: Texture | Texture[] = useLoader(loader, isCubeMap ? [files] : files, (loader) => {
     loader.setPath(path)
+    if (!isCubeMap) (loader as RGBELoader).setDataType(FloatType)
     if (extensions) extensions(loader)
   })
-  const map: Texture = isCubeMap ? loaderResult[0] : loaderResult
-
-  // PMREMGenerator takes its sweet time to generate the env-map,
-  // Let's make this part of suspense, or else it just yields a frame-skip
-  const texture = useAsset<Texture, [Texture]>(
-    () =>
-      new Promise((res) => {
-        const gen = new PMREMGenerator(gl)
-        const texture = getTexture(map, gen, isCubeMap) as Texture
-        gen.dispose()
-        res(texture)
-      }),
-    map
-  )
+  const texture: Texture = isCubeMap ? loaderResult[0] : loaderResult
+  texture.mapping = isCubeMap ? CubeRefractionMapping : EquirectangularReflectionMapping
 
   React.useLayoutEffect(() => {
     const oldbg = scene ? scene.background : defaultScene.background
@@ -82,8 +68,7 @@ export function Environment({
         defaultScene.environment = oldenv
         defaultScene.background = oldbg
       }
-      // Environment textures are volatile, better dispose and uncache them
-      useAsset.clear(map)
+      // Environment textures are volatile, better dispose them
       texture.dispose()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps

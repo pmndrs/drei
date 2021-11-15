9da10c1b8af4d7daf5c0836e42315c0d529638f0
Paul Henschel <drcmda@gmail.com>
2021-11-15 12:21:41 +0100

    feat: deprecate depthbuffer and add usedepthbuffer

--- src/core/useDepthBuffer.ts
+++ src/core/useDepthBuffer.ts
@@ -3,12 +3,7 @@ import * as React from 'react'
 import { useThree, useFrame } from '@react-three/fiber'
 import { useFBO } from './useFBO'
 
-const DepthBuffer = React.forwardRef(({ size = 256 }: { size?: number }, ref) => {
-  React.useEffect(() => {
-    console.warn(
-      'DepthBuffer has been deprecated and will be removed next major. Replace it with useDepthBuffer(size)!'
-    )
-  }, [])
+function useDepthBuffer(size = 256) {
   const dpr = useThree((state) => state.viewport.dpr)
   const { width, height } = useThree((state) => state.size)
   const w = size || width * dpr
@@ -22,12 +17,12 @@ const DepthBuffer = React.forwardRef(({ size = 256 }: { size?: number }, ref) =>
   }, [w, h])
 
   const depthFBO = useFBO(w, h, depthConfig)
-  React.useImperativeHandle(ref, () => depthFBO?.depthTexture)
-  return useFrame((state) => {
+  useFrame((state) => {
     state.gl.setRenderTarget(depthFBO)
     state.gl.render(state.scene, state.camera)
     state.gl.setRenderTarget(null)
   })
-})
+  return depthFBO.depthTexture
+}
 
-export { DepthBuffer }
+export { useDepthBuffer }

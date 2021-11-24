a09bf023a39e50d2fd0b3c64b12e5d9f58dae687
— <drcmda@gmail.com>
2021-11-24 23:30:15 +0100

    fix: cloud

--- src/core/Cloud.tsx
+++ src/core/Cloud.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Group, Texture } from 'three'
 import { useFrame } from '@react-three/fiber'
 import { Billboard } from './Billboard'
+import { Plane } from './shapes'
 import { useTexture } from './useTexture'
 import CloudImage from '../assets/cloud.base64'
 
@@ -21,8 +22,8 @@ export function Cloud({ opacity = 0.5, speed = 0.4, width = 10, length = 1.5, se
   )
   useFrame((state) =>
     group.current?.children.forEach((cloud, index) => {
-      cloud.rotation.z += clouds[index].rotation * dir
-      cloud.scale.setScalar(
+      cloud.children[0].rotation.z += clouds[index].rotation * dir
+      cloud.children[0].scale.setScalar(
         clouds[index].scale + (((1 + Math.sin(state.clock.getElapsedTime() / 10)) / 2) * index) / 10
       )
     })
@@ -31,13 +32,15 @@ export function Cloud({ opacity = 0.5, speed = 0.4, width = 10, length = 1.5, se
     <group {...props}>
       <group position={[0, 0, (segments / 2) * length]} ref={group}>
         {clouds.map(({ x, y, scale, density }, index) => (
-          <Billboard key={index} scale={[scale, scale, scale]} position={[x, y, -index * length]} lockZ>
-            <meshStandardMaterial
-              map={texture}
-              transparent
-              opacity={(scale / 6) * density * opacity}
-              depthTest={false}
-            />
+          <Billboard key={index} position={[x, y, -index * length]}>
+            <Plane scale={scale} rotation={[0, 0, 0]}>
+              <meshStandardMaterial
+                map={texture}
+                transparent
+                opacity={(scale / 6) * density * opacity}
+                depthTest={false}
+              />
+            </Plane>
           </Billboard>
         ))}
       </group>

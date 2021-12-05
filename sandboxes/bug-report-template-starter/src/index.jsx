2113ff704df637890fc621c93d4328d2dd4d9d2b
Bjorn Stromberg <bjorn.stromberg@klarna.com>
2021-12-05 12:36:29 +0100

    chore: use sandbox from repo for bug report template (#671)

---
+++ sandboxes/bug-report-template-starter/src/index.jsx
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react'
+import ReactDOM from 'react-dom'
+import { Canvas, useFrame } from '@react-three/fiber'
+import './styles.css'
+
+function Thing() {
+  const ref = useRef(null)
+  useFrame(() => {
+    ref.current.rotation.x = ref.current.rotation.y += 0.01
+  })
+  return (
+    <mesh
+      ref={ref}
+      onClick={(e) => console.log('click')}
+      onPointerOver={(e) => console.log('hover')}
+      onPointerOut={(e) => console.log('unhover')}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  )
+}
+
+ReactDOM.render(
+  <Canvas>
+    <Thing />
+  </Canvas>,
+  document.getElementById('root')
+)

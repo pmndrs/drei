aa56ab27f0cb5d2c9ebbcbaf7293e24881cfddd7
Gianmarco <gianmarcosimone89@gmail.com>
2021-12-14 17:52:45 +0100

    feat: removes lookup jsons from repo, using drei-assets instead

--- src/core/useNormalTexture.tsx
+++ src/core/useNormalTexture.tsx
@@ -2,10 +2,10 @@ import * as React from 'react'
 import { useTexture } from './useTexture'
 import { RepeatWrapping, Texture, Vector2 } from 'three'
 
-import { normalsList } from '../helpers/normal-assets'
+import { useAsset } from 'use-asset'
 
-const NORMAL_ROOT = 'https://rawcdn.githack.com/emmelleppi/normal-maps/f24c810fc1d86b5b1e5dfea914b668f70b5f2923'
-const DEFAULT_NORMAL = normalsList[0]
+const NORMAL_ROOT = 'https://rawcdn.githack.com/pmndrs/drei-assets/7a3104997e1576f83472829815b00880d88b32fb'
+const LIST_URL = 'https://cdn.jsdelivr.net/gh/pmndrs/drei-assets@master/normals/normals.json'
 
 type Settings = {
   repeat?: number[]
@@ -16,7 +16,16 @@ type Settings = {
 export function useNormalTexture(id = 0, settings: Settings = {}): [Texture, string, number] {
   const { repeat = [1, 1], anisotropy = 1, offset = [0, 0] } = settings
 
+  const normalsList = useAsset<Record<string, string>, [string]>(
+    () =>
+      new Promise(async (resolve) => {
+        const normalsList = await fetch(LIST_URL).then((res) => res.json())
+        resolve(normalsList)
+      }),
+    'normalsList'
+  )
   const numTot = React.useMemo(() => Object.keys(normalsList).length, [])
+  const DEFAULT_NORMAL = normalsList[0]
 
   const imageName = normalsList[id] || DEFAULT_NORMAL
   const url = `${NORMAL_ROOT}/normals/${imageName}`

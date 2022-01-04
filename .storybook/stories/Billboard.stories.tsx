67ed4a448f594355fa99100871a4977276de96fc
Bjorn Stromberg <bjorn.stromberg@klarna.com>
2022-01-04 18:38:03 +0100

    chore: migrate storybooks to use controls instead of knobs (#686)

--- .storybook/stories/Billboard.stories.tsx
+++ .storybook/stories/Billboard.stories.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { withKnobs, boolean } from '@storybook/addon-knobs'
 import { Vector3 } from 'three'
 
 import { Setup } from '../Setup'
@@ -15,79 +14,76 @@ export default {
         {storyFn()}
       </Setup>
     ),
-    withKnobs,
   ],
 }
 
-function BillboardStory() {
-  const follow = boolean('follow', true)
-  const lockX = boolean('lockX', false)
-  const lockY = boolean('lockY', false)
-  const lockZ = boolean('lockZ', false)
+export const BillboardStory = ({ follow, lockX, lockY, lockZ }) => (
+  <>
+    <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[-4, -2, 0]}>
+      <Plane args={[3, 2]} material-color="red" />
+    </Billboard>
+    <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[-4, 2, 0]}>
+      <Plane args={[3, 2]} material-color="orange" />
+    </Billboard>
+    <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[0, 0, 0]}>
+      <Plane args={[3, 2]} material-color="green" />
+    </Billboard>
+    <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[4, -2, 0]}>
+      <Plane args={[3, 2]} material-color="blue" />
+    </Billboard>
+    <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[4, 2, 0]}>
+      <Plane args={[3, 2]} material-color="yellow" />
+    </Billboard>
 
-  return (
-    <>
-      <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[-4, -2, 0]}>
-        <Plane args={[3, 2]} material-color="red" />
-      </Billboard>
-      <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[-4, 2, 0]}>
-        <Plane args={[3, 2]} material-color="orange" />
-      </Billboard>
-      <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[0, 0, 0]}>
-        <Plane args={[3, 2]} material-color="green" />
-      </Billboard>
-      <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[4, -2, 0]}>
-        <Plane args={[3, 2]} material-color="blue" />
-      </Billboard>
-      <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[4, 2, 0]}>
-        <Plane args={[3, 2]} material-color="yellow" />
-      </Billboard>
+    <OrbitControls enablePan={true} zoomSpeed={0.5} />
+  </>
+)
 
-      <OrbitControls enablePan={true} zoomSpeed={0.5} />
-    </>
-  )
+BillboardStory.args = {
+  follow: true,
+  lockX: false,
+  lockY: false,
+  lockZ: false,
 }
 
-function BillboardTextStory() {
-  const follow = boolean('follow', true)
-  const lockX = boolean('lockX', false)
-  const lockY = boolean('lockY', false)
-  const lockZ = boolean('lockZ', false)
+BillboardStory.storyName = 'Planes'
 
-  return (
-    <>
-      <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[0.5, 2.05, 0.5]}>
+export const BillboardTextStory = ({ follow, lockX, lockY, lockZ }) => (
+  <>
+    <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[0.5, 2.05, 0.5]}>
+      <Text fontSize={1} outlineWidth={'5%'} outlineColor="#000000" outlineOpacity={1}>
+        box
+      </Text>
+    </Billboard>
+    <Box position={[0.5, 1, 0.5]}>
+      <meshStandardMaterial color="red" />
+    </Box>
+    <group position={[-2.5, -3, -1]}>
+      <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[0, 1.05, 0]}>
         <Text fontSize={1} outlineWidth={'5%'} outlineColor="#000000" outlineOpacity={1}>
-          box
+          cone
         </Text>
       </Billboard>
-      <Box position={[0.5, 1, 0.5]}>
-        <meshStandardMaterial color="red" />
-      </Box>
-      <group position={[-2.5, -3, -1]}>
-        <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[0, 1.05, 0]}>
-          <Text fontSize={1} outlineWidth={'5%'} outlineColor="#000000" outlineOpacity={1}>
-            cone
-          </Text>
-        </Billboard>
-        <Cone>
-          <meshStandardMaterial color="green" />
-        </Cone>
-      </group>
+      <Cone>
+        <meshStandardMaterial color="green" />
+      </Cone>
+    </group>
 
-      <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[0, 0, -5]}>
-        <Plane args={[2, 2]}>
-          <meshStandardMaterial color="#000066" />
-        </Plane>
-      </Billboard>
+    <Billboard follow={follow} lockX={lockX} lockY={lockY} lockZ={lockZ} position={[0, 0, -5]}>
+      <Plane args={[2, 2]}>
+        <meshStandardMaterial color="#000066" />
+      </Plane>
+    </Billboard>
 
-      <OrbitControls enablePan={true} zoomSpeed={0.5} />
-    </>
-  )
+    <OrbitControls enablePan={true} zoomSpeed={0.5} />
+  </>
+)
+
+BillboardTextStory.args = {
+  follow: true,
+  lockX: false,
+  lockY: false,
+  lockZ: false,
 }
 
-export const BillboardSt = () => <BillboardStory />
-BillboardSt.storyName = 'Planes'
-
-export const BillboardTextSt = () => <BillboardTextStory />
-BillboardTextSt.storyName = 'Text'
+BillboardTextStory.storyName = 'Text'

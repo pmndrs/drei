697790ff2b4e91803f95553071d6077b44fa29f9
— <drcmda@gmail.com>
2022-01-22 09:29:41 +0100

    fix: apparently premgenerator is not needed in threejs any longer

--- src/core/Environment.tsx
+++ src/core/Environment.tsx
@@ -1,18 +1,9 @@
 import * as React from 'react'
 import { useLoader, useThree } from '@react-three/fiber'
-import { CubeTexture, CubeTextureLoader, Texture, PMREMGenerator, Scene, Loader } from 'three'
+import { FloatType, EquirectangularReflectionMapping, CubeTextureLoader, Texture, Scene, Loader } from 'three'
 import { RGBELoader } from 'three-stdlib'
-import { useAsset } from 'use-asset'
 
 import { presetsObj, PresetsType } from '../helpers/environment-assets'
-
-function getTexture(texture: Texture | CubeTexture, gen: PMREMGenerator, isCubeMap: boolean) {
-  if (isCubeMap) {
-    gen.compileEquirectangularShader()
-    return gen.fromCubemap(texture as CubeTexture).texture
-  }
-  return gen.fromEquirectangular(texture).texture
-}
 
 const CUBEMAP_ROOT = 'https://rawcdn.githack.com/pmndrs/drei-assets/aa3600359ba664d546d05821bcbca42013587df2'
 
@@ -41,28 +32,17 @@ export function Environment({
     path = CUBEMAP_ROOT + '/hdri/'
   }
   const defaultScene = useThree(({ scene }) => scene)
-  const gl = useThree(({ gl }) => gl)
   const isCubeMap = Array.isArray(files)
   const loader = isCubeMap ? CubeTextureLoader : RGBELoader
   // @ts-expect-error
   const loaderResult: Texture | Texture[] = useLoader(loader, isCubeMap ? [files] : files, (loader) => {
     loader.setPath(path)
+    // @ts-expect-error
+    loader.setDataType?.(FloatType)
     if (extensions) extensions(loader)
   })
-  const map: Texture = isCubeMap ? loaderResult[0] : loaderResult
-
-  // PMREMGenerator takes its sweet time to generate the env-map,
-  // Let's make this part of suspense, or else it just yields a frame-skip
-  const texture = useAsset<Texture, [Texture]>(
-    () =>
-      new Promise((res) => {
-        const gen = new PMREMGenerator(gl)
-        const texture = getTexture(map, gen, isCubeMap) as Texture
-        gen.dispose()
-        res(texture)
-      }),
-    map
-  )
+  const texture: Texture = isCubeMap ? loaderResult[0] : loaderResult
+  texture.mapping = EquirectangularReflectionMapping
 
   React.useLayoutEffect(() => {
     const oldbg = scene ? scene.background : defaultScene.background
@@ -83,7 +63,6 @@ export function Environment({
         defaultScene.background = oldbg
       }
       // Environment textures are volatile, better dispose and uncache them
-      useAsset.clear(map)
       texture.dispose()
     }
   }, [texture, background, scene])

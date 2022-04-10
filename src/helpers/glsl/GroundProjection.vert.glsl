4e454969e2bebebbe2b9da2098569af43d2e208f
Faraz Shaikh <farazzshaikh@gmail.com>
2022-04-10 19:35:51 +0530

    feat: Ground projection for Env maps (#855)
    * feat: Ground projection for Env maps
    
    * chore: Update readme for ground projection

---
+++ src/helpers/glsl/GroundProjection.vert.glsl
@@ -0,0 +1,7 @@
+varying vec3 vWorldPosition;
+
+void main() {
+  vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+  vWorldPosition = worldPosition.xyz;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}

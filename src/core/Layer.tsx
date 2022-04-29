b5d7d778d0a381eea1ff1cd1728c0ca79a4f2224
CoolAbhi1290 <coolabhigyaandeep07@gmail.com>
2022-04-29 01:01:16 +0000

    fix: tweaked Layer to work in Drei context

--- src/core/Layer.tsx
+++ src/core/Layer.tsx
@@ -1,37 +1,36 @@
-import { useFrame } from "@react-three/fiber";
-import { FC, ReactNode, useRef } from "react";
-import { Scene } from "three";
+import { useFrame } from '@react-three/fiber'
+import * as React from 'react'
+import { Scene } from 'three'
 
 interface LayerProps {
-  layer: number;
-  children: ReactNode;
+  layer: number
+  children: React.ReactNode
 }
 /**
  * Renders children in a predictable order. Some may render above others by
  * having a higher layer number.
- * 
+ *
  * @example
  * ```typescript
  * // will render above backgound component
  * <Layer layer={2}>
  *   <MyText />
  * </Layer>
- * 
+ *
  * // below the text as it is rendered first/low layer
  * <Layer layer={1}>
  *   <MyBackground />
  * </Layer>
  * ```
  */
-const Layer: FC<LayerProps> = ({ layer, children }) => {
-  const sceneRef = useRef<Scene>(null!);
+export const Layer: React.FC<LayerProps> = ({ layer, children }) => {
+  const sceneRef = React.useRef<Scene>(null!)
 
   useFrame(({ gl, camera }) => {
-    gl.autoClear = false;
-    gl.clearDepth();
-    gl.render(sceneRef.current, camera);
-  }, layer);
+    gl.autoClear = false
+    gl.clearDepth()
+    gl.render(sceneRef.current, camera)
+  }, layer)
 
-  return <scene ref={sceneRef}>{children}</scene>;
-};
-  
+  return <scene ref={sceneRef}>{children}</scene>
+}

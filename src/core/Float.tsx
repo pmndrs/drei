2c9729ee10bf4d44d0bc2e099c3333b5712c2b03
unknown <1starwarrior516@gmail.com>
2022-05-12 01:21:43 +0530

    feat: added range to Float position

--- src/core/Float.tsx
+++ src/core/Float.tsx
@@ -1,16 +1,30 @@
 import * as React from 'react'
 import { useFrame } from '@react-three/fiber'
 import mergeRefs from 'react-merge-refs'
+import * as THREE from 'three'
 
 export type FloatProps = JSX.IntrinsicElements['group'] & {
   speed?: number
   rotationIntensity?: number
   floatIntensity?: number
   children?: React.ReactNode
+  minPosition?: number
+  maxPosition?: number
 }
 
 export const Float = React.forwardRef<THREE.Group, FloatProps>(
-  ({ children, speed = 1, rotationIntensity = 1, floatIntensity = 1, ...props }, forwardRef) => {
+  (
+    {
+      children,
+      speed = 1,
+      rotationIntensity = 1,
+      floatIntensity = 1,
+      minPosition = -1 * floatIntensity,
+      maxPosition = floatIntensity,
+      ...props
+    },
+    forwardRef
+  ) => {
     const ref = React.useRef<THREE.Group>(null!)
     const offset = React.useRef(Math.random() * 10000)
     useFrame((state) => {
@@ -18,7 +32,15 @@ export const Float = React.forwardRef<THREE.Group, FloatProps>(
       ref.current.rotation.x = (Math.cos((t / 4) * speed) / 8) * rotationIntensity
       ref.current.rotation.y = (Math.sin((t / 4) * speed) / 8) * rotationIntensity
       ref.current.rotation.z = (Math.sin((t / 4) * speed) / 20) * rotationIntensity
-      ref.current.position.y = (Math.sin((t / 4) * speed) / 10) * floatIntensity
+      let yPosition = (Math.sin((t / 4) * speed) / 10) * floatIntensity
+      yPosition = THREE.MathUtils.mapLinear(
+        yPosition,
+        -0.1 * floatIntensity,
+        floatIntensity * 0.1,
+        minPosition,
+        maxPosition
+      )
+      ref.current.position.y = yPosition
     })
     return (
       <group {...props}>

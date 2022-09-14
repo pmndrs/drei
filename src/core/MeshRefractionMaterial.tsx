3cc7caa7d722c9a873c528401af52ac6d179eaaa
— <drcmda@gmail.com>
2022-09-14 23:38:53 +0200

    fix: refrmaterial takes sampler2d and samplercube

--- src/core/MeshRefractionMaterial.tsx
+++ src/core/MeshRefractionMaterial.tsx
@@ -6,7 +6,6 @@ import * as THREE from 'three'
 import * as React from 'react'
 import { useLayoutEffect, useMemo, useRef } from 'react'
 import { extend, ReactThreeFiber, useThree } from '@react-three/fiber'
-import { shaderMaterial } from './shaderMaterial'
 import { MeshBVH, SAH } from 'three-mesh-bvh'
 import { MeshRefractionMaterial as MeshRefractionMaterial_ } from '../materials/MeshRefractionMaterial'
 
@@ -25,11 +24,14 @@ type MeshRefractionMaterialProps = JSX.IntrinsicElements['shaderMaterial'] & {
   aberrationStrength?: number
   color?: ReactThreeFiber.Color
   resolution?: ReactThreeFiber.Vector2
-  envMap?: THREE.Texture
+  envMap?: THREE.CubeTexture | THREE.Texture
   frames?: number
   near?: number
   far?: number
 }
+
+const isCubeTexture = (def: THREE.CubeTexture | THREE.Texture): def is THREE.CubeTexture =>
+  def && (def as THREE.CubeTexture).isCubeTexture
 
 export function MeshRefractionMaterial({
   frames = 1,
@@ -37,14 +39,30 @@ export function MeshRefractionMaterial({
   near = 0.1,
   far = 1000,
   aberrationStrength = 0,
+  envMap,
   ...props
 }: MeshRefractionMaterialProps) {
   extend({ MeshRefractionMaterial: MeshRefractionMaterial_ })
 
   const material = useRef()
   const { size } = useThree()
+
   const defines = useMemo(() => {
     const temp = {} as { [key: string]: string }
+    if (envMap) {
+      const isCubeMap = isCubeTexture(envMap)
+      const w = (isCubeMap ? envMap.image[0]?.width : envMap.image.width) ?? 1024
+      const cubeSize = w / 4
+      const _lodMax = Math.floor(Math.log2(cubeSize))
+      const _cubeSize = Math.pow(2, _lodMax)
+      const width = 3 * Math.max(_cubeSize, 16 * 7)
+      const height = 4 * _cubeSize
+
+      if (isCubeMap) temp.ENVMAP_TYPE_CUBEM = ''
+      temp.CUBEUV_TEXEL_WIDTH = `${1.0 / width}`
+      temp.CUBEUV_TEXEL_HEIGHT = `${1.0 / height}`
+      temp.CUBEUV_MAX_MIP = `${_lodMax}.0`
+    }
     if (aberrationStrength > 0) temp.CHROMATIC_ABERRATIONS = ''
     return temp
   }, [aberrationStrength])
@@ -66,6 +84,7 @@ export function MeshRefractionMaterial({
       ref={material}
       resolution={[size.width, size.height]}
       aberrationStrength={aberrationStrength}
+      envMap={envMap}
       {...props}
     />
   )

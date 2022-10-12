0bb04370f4d3535944d67ffeea6fd1451ef3d870
rodrigo <hamuyrodrigo@gmail.com>
2022-10-12 09:27:58 +0100

    docs: add more props to Svg story

--- .storybook/stories/Svg.stories.tsx
+++ .storybook/stories/Svg.stories.tsx
@@ -1,25 +1,86 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { useEffect } from '@storybook/addons'
+import { useArgs } from '@storybook/client-api'
+import { ComponentMeta } from '@storybook/react'
 import * as React from 'react'
-import { Suspense } from 'react'
-import { Vector3 } from 'three'
+import { ComponentProps, FC, Suspense } from 'react'
+import { NoToneMapping, Vector3 } from 'three'
 import { Svg } from '../../src'
 import { Setup } from '../Setup'
+
+const svgRecord = {
+  Tiger: 'models/svg/tiger.svg',
+  'Three.js': 'models/svg/threejs.svg',
+  'Joins and caps': 'models/svg/lineJoinsAndCaps.svg',
+  Hexagon: 'models/svg/hexagon.svg',
+  Energy: 'models/svg/energy.svg',
+  'Test 1': 'models/svg/tests/1.svg',
+  'Test 2': 'models/svg/tests/2.svg',
+  'Test 3': 'models/svg/tests/3.svg',
+  'Test 4': 'models/svg/tests/4.svg',
+  'Test 5': 'models/svg/tests/5.svg',
+  'Test 6': 'models/svg/tests/6.svg',
+  'Test 7': 'models/svg/tests/7.svg',
+  'Test 8': 'models/svg/tests/8.svg',
+  'Test 9': 'models/svg/tests/9.svg',
+  Units: 'models/svg/tests/units.svg',
+  Ordering: 'models/svg/tests/ordering.svg',
+  Defs: 'models/svg/tests/testDefs/Svg-defs.svg',
+  Defs2: 'models/svg/tests/testDefs/Svg-defs2.svg',
+  Defs3: 'models/svg/tests/testDefs/Wave-defs.svg',
+  Defs4: 'models/svg/tests/testDefs/defs4.svg',
+  Defs5: 'models/svg/tests/testDefs/defs5.svg',
+  'Style CSS inside defs': 'models/svg/style-css-inside-defs.svg',
+  'Multiple CSS classes': 'models/svg/multiple-css-classes.svg',
+  'Zero Radius': 'models/svg/zero-radius.svg',
+  'Styles in svg tag': 'models/svg/tests/styles.svg',
+  'Round join': 'models/svg/tests/roundJoinPrecisionIssue.svg',
+}
+
+const url = 'https://threejs.org/examples'
 
 export default {
   title: 'Abstractions/Svg',
   component: Svg,
-  decorators: [(storyFn) => <Setup cameraPosition={new Vector3(0, 0, 200)}>{storyFn()}</Setup>],
+  decorators: [
+    (storyFn) => (
+      <Setup
+        gl={{ toneMapping: NoToneMapping }}
+        onCreated={(st) => st.gl.setClearColor('#ccc')}
+        cameraPosition={new Vector3(0, 0, 200)}
+        lights={false}
+      >
+        {storyFn()}
+      </Setup>
+    ),
+  ],
   args: {
-    src: 'https://threejs.org/examples/models/svg/tiger.svg',
+    src: `${url}/${svgRecord.Tiger}`,
+    svg: svgRecord.Tiger,
     skipFill: false,
     skipStrokes: false,
     strokesWireframe: false,
     fillWireframe: false,
   },
+  argTypes: {
+    svg: {
+      options: svgRecord,
+      control: {
+        type: 'select',
+      },
+    },
+  },
 } as ComponentMeta<typeof Svg>
 
-export const SvgSt: ComponentStory<typeof Svg> = (props) => (
-  <Suspense fallback={null}>
-    <Svg {...props} position={[-300, 300, -300]} />
-  </Suspense>
-)
+export const SvgSt: FC<{ svg: string } & ComponentProps<typeof Svg>> = ({ svg, ...props }) => {
+  const [_, updateArgs] = useArgs()
+  useEffect(() => {
+    console.log(svg)
+    updateArgs({ src: `${url}/${svg || svgRecord.Tiger}` })
+  }, [svg])
+  return (
+    <Suspense fallback={null}>
+      <Svg {...props} position={[-70, 70, 0]} scale={0.25} />
+      <axesHelper />
+    </Suspense>
+  )
+}

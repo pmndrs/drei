986eb9b33957e7128c6e21f79fe1de6af0d25816
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-11-13 14:19:18 -0600

    fix: remove IIFE in SDFGenerator

--- src/core/Text/troika-three-text/SDFGenerator.js
+++ src/core/Text/troika-three-text/SDFGenerator.js
@@ -33,95 +33,94 @@ export function generateSDF(width, height, path, viewBox, distance, exponent, ca
  * WebGL-based implementation executed on the main thread. Requests are executed in time-bounded
  * macrotask chunks to allow render frames to execute in between.
  */
-const generateSDF_GL = /*#__PURE__*/ (function () {
-  const queue = []
-  const chunkTimeBudget = 5 //ms
-  let timer = 0
-  function nextChunk() {
-    const start = now()
-    while (queue.length && now() - start < chunkTimeBudget) {
-      queue.shift()()
-    }
-    timer = queue.length ? setTimeout(nextChunk, 0) : 0
+const queue = []
+const chunkTimeBudget = 5 //ms
+let timer = 0
+
+function nextChunk() {
+  const start = now()
+  while (queue.length && now() - start < chunkTimeBudget) {
+    queue.shift()()
   }
-  return (...args) => {
-    return new Promise((resolve, reject) => {
-      queue.push(() => {
-        const start = now()
-        try {
-          mainThreadGenerator.webgl.generateIntoCanvas(...args)
-          resolve({ timing: now() - start })
-        } catch (err) {
-          reject(err)
-        }
-      })
-      if (!timer) {
-        timer = setTimeout(nextChunk, 0)
+  timer = queue.length ? setTimeout(nextChunk, 0) : 0
+}
+
+const generateSDF_GL = (...args) => {
+  return new Promise((resolve, reject) => {
+    queue.push(() => {
+      const start = now()
+      try {
+        mainThreadGenerator.webgl.generateIntoCanvas(...args)
+        resolve({ timing: now() - start })
+      } catch (err) {
+        reject(err)
       }
     })
-  }
-})()
+    if (!timer) {
+      timer = setTimeout(nextChunk, 0)
+    }
+  })
+}
 
 /**
  * Fallback JS-based implementation, fanned out to a number of worker threads for parallelism
  */
-const generateSDF_JS_Worker = /*#__PURE__*/ (function () {
-  const threadCount = 4 //how many workers to spawn
-  const idleTimeout = 2000 //workers will be terminated after being idle this many milliseconds
-  const threads = {}
-  let callNum = 0
-  return function (width, height, path, viewBox, distance, exponent, canvas, x, y, channel) {
-    const workerId = 'TroikaTextSDFGenerator_JS_' + (callNum++ % threadCount)
-    let thread = threads[workerId]
-    if (!thread) {
-      thread = threads[workerId] = {
-        workerModule: defineWorkerModule({
-          name: workerId,
-          workerId,
-          dependencies: [createSDFGenerator, now],
-          init(_createSDFGenerator, now) {
-            const generate = _createSDFGenerator().javascript.generate
-            return function (...args) {
-              const start = now()
-              const textureData = generate(...args)
-              return {
-                textureData,
-                timing: now() - start,
-              }
+const threadCount = 4 // how many workers to spawn
+const idleTimeout = 2000 // workers will be terminated after being idle this many milliseconds
+const threads = {}
+let callNum = 0
+
+function generateSDF_JS_Worker(width, height, path, viewBox, distance, exponent, canvas, x, y, channel) {
+  const workerId = 'TroikaTextSDFGenerator_JS_' + (callNum++ % threadCount)
+  let thread = threads[workerId]
+  if (!thread) {
+    thread = threads[workerId] = {
+      workerModule: defineWorkerModule({
+        name: workerId,
+        workerId,
+        dependencies: [createSDFGenerator, now],
+        init(_createSDFGenerator, now) {
+          const generate = _createSDFGenerator().javascript.generate
+          return function (...args) {
+            const start = now()
+            const textureData = generate(...args)
+            return {
+              textureData,
+              timing: now() - start,
             }
-          },
-          getTransferables(result) {
-            return [result.textureData.buffer]
-          },
-        }),
-        requests: 0,
-        idleTimer: null,
-      }
+          }
+        },
+        getTransferables(result) {
+          return [result.textureData.buffer]
+        },
+      }),
+      requests: 0,
+      idleTimer: null,
     }
-
-    thread.requests++
-    clearTimeout(thread.idleTimer)
-    return thread.workerModule(width, height, path, viewBox, distance, exponent).then(({ textureData, timing }) => {
-      // copy result data into the canvas
-      const start = now()
-      // expand single-channel data into rgba
-      const imageData = new Uint8Array(textureData.length * 4)
-      for (let i = 0; i < textureData.length; i++) {
-        imageData[i * 4 + channel] = textureData[i]
-      }
-      mainThreadGenerator.webglUtils.renderImageData(canvas, imageData, x, y, width, height, 1 << (3 - channel))
-      timing += now() - start
-
-      // clean up workers after a while
-      if (--thread.requests === 0) {
-        thread.idleTimer = setTimeout(() => {
-          terminateWorker(workerId)
-        }, idleTimeout)
-      }
-      return { timing }
-    })
   }
-})()
+
+  thread.requests++
+  clearTimeout(thread.idleTimer)
+  return thread.workerModule(width, height, path, viewBox, distance, exponent).then(({ textureData, timing }) => {
+    // copy result data into the canvas
+    const start = now()
+    // expand single-channel data into rgba
+    const imageData = new Uint8Array(textureData.length * 4)
+    for (let i = 0; i < textureData.length; i++) {
+      imageData[i * 4 + channel] = textureData[i]
+    }
+    mainThreadGenerator.webglUtils.renderImageData(canvas, imageData, x, y, width, height, 1 << (3 - channel))
+    timing += now() - start
+
+    // clean up workers after a while
+    if (--thread.requests === 0) {
+      thread.idleTimer = setTimeout(() => {
+        terminateWorker(workerId)
+      }, idleTimeout)
+    }
+    return { timing }
+  })
+}
 
 export function warmUpSDFCanvas(canvas) {
   if (!canvas._warm) {

4dd9d13d92d786a1f1ba3bb0d39991afd2410d7c
— <drcmda@gmail.com>
2023-03-30 12:29:33 +0200

    fix: edges forwardref

--- src/core/Edges.tsx
+++ src/core/Edges.tsx
@@ -7,22 +7,28 @@ type Props = JSX.IntrinsicElements['lineSegments'] & {
   color?: ReactThreeFiber.Color
 }
 
-export function Edges({ userData, children, geometry, threshold = 15, color = 'black', ...props }: Props) {
-  const ref = React.useRef<THREE.LineSegments>(null!)
-  React.useLayoutEffect(() => {
-    const parent = ref.current.parent as THREE.Mesh
-    if (parent) {
-      const geom = geometry || parent.geometry
-      if (geom !== ref.current.userData.currentGeom || threshold !== ref.current.userData.currentThreshold) {
-        ref.current.userData.currentGeom = geom
-        ref.current.userData.currentThreshold = threshold
-        ref.current.geometry = new THREE.EdgesGeometry(geom, threshold)
+export const Edges = React.forwardRef(
+  (
+    { userData, children, geometry, threshold = 15, color = 'black', ...props }: Props,
+    fref: React.ForwardedRef<THREE.LineSegments>
+  ) => {
+    const ref = React.useRef<THREE.LineSegments>(null!)
+    React.useLayoutEffect(() => {
+      const parent = ref.current.parent as THREE.Mesh
+      if (parent) {
+        const geom = geometry || parent.geometry
+        if (geom !== ref.current.userData.currentGeom || threshold !== ref.current.userData.currentThreshold) {
+          ref.current.userData.currentGeom = geom
+          ref.current.userData.currentThreshold = threshold
+          ref.current.geometry = new THREE.EdgesGeometry(geom, threshold)
+        }
       }
-    }
-  })
-  return (
-    <lineSegments ref={ref} raycast={() => null} {...props}>
-      {children ? children : <lineBasicMaterial color={color} />}
-    </lineSegments>
-  )
-}
+    })
+    React.useImperativeHandle(fref, () => ref.current)
+    return (
+      <lineSegments ref={ref} raycast={() => null} {...props}>
+        {children ? children : <lineBasicMaterial color={color} />}
+      </lineSegments>
+    )
+  }
+)

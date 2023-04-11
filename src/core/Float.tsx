1a3564ef293bc1fe79e99a3b70c7260c2e4077b6
Clément Roche <rchclement@gmail.com>
2023-04-12 01:50:05 +0200

    Add "enabled" prop and update matrix manually (#1397)
    - "enabled" prop can be used to prevent computation every frame if not needed. It's also disabled if speed equals 0
    - update matrix manually in order to improve performance if float is not enabled

--- src/core/Float.tsx
+++ src/core/Float.tsx
@@ -4,6 +4,7 @@ import mergeRefs from 'react-merge-refs'
 import * as THREE from 'three'
 
 export type FloatProps = JSX.IntrinsicElements['group'] & {
+  enabled?: boolean
   speed?: number
   rotationIntensity?: number
   floatIntensity?: number
@@ -13,12 +14,13 @@ export type FloatProps = JSX.IntrinsicElements['group'] & {
 
 export const Float = React.forwardRef<THREE.Group, FloatProps>(
   (
-    { children, speed = 1, rotationIntensity = 1, floatIntensity = 1, floatingRange = [-0.1, 0.1], ...props },
+    { children, enabled = true, speed = 1, rotationIntensity = 1, floatIntensity = 1, floatingRange = [-0.1, 0.1], ...props },
     forwardRef
   ) => {
     const ref = React.useRef<THREE.Group>(null!)
     const offset = React.useRef(Math.random() * 10000)
     useFrame((state) => {
+      if(!enabled || speed === 0) return
       const t = offset.current + state.clock.getElapsedTime()
       ref.current.rotation.x = (Math.cos((t / 4) * speed) / 8) * rotationIntensity
       ref.current.rotation.y = (Math.sin((t / 4) * speed) / 8) * rotationIntensity
@@ -26,10 +28,11 @@ export const Float = React.forwardRef<THREE.Group, FloatProps>(
       let yPosition = Math.sin((t / 4) * speed) / 10
       yPosition = THREE.MathUtils.mapLinear(yPosition, -0.1, 0.1, floatingRange?.[0] ?? -0.1, floatingRange?.[1] ?? 0.1)
       ref.current.position.y = yPosition * floatIntensity
+      ref.current.updateMatrix()
     })
     return (
       <group {...props}>
-        <group ref={mergeRefs([ref, forwardRef])}>{children}</group>
+        <group ref={mergeRefs([ref, forwardRef])} matrixAutoUpdate={false}>{children}</group>
       </group>
     )
   }

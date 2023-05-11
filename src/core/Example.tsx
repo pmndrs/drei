3637e8c7868825e69688b99fc8f663713e90b4f8
Antoine BERNIER <antoine.bernier@gmail.com>
2023-05-11 14:50:39 +0200

    feat: `Example` component (#1439)
    * docs(Example): an Example component
    
    * docs: inner-links

---
+++ src/core/Example.tsx
@@ -0,0 +1,47 @@
+/* eslint react-hooks/exhaustive-deps: 1 */
+import * as React from 'react'
+import * as THREE from 'three'
+import { type Color } from '@react-three/fiber'
+
+import { Text3D } from './Text3D'
+import { Center } from './Center'
+
+export type ExampleProps = {
+  color?: Color
+  debug?: boolean
+  bevelSize?: number
+  font?: string
+} & JSX.IntrinsicElements['group']
+
+export type ExampleApi = {
+  incr: (x?: number) => void
+  decr: (x?: number) => void
+}
+
+export const Example = React.forwardRef<ExampleApi, ExampleProps>(
+  (
+    { font = '/fonts/Inter_Bold.json', color = '#cbcbcb', bevelSize = 0.04, debug = false, children, ...props },
+    fref
+  ) => {
+    const [counter, setCounter] = React.useState(0)
+
+    const incr = React.useCallback((x = 1) => setCounter(counter + x), [counter])
+    const decr = React.useCallback((x = 1) => setCounter(counter - x), [counter])
+
+    // ref-API
+    const api = React.useMemo<ExampleApi>(() => ({ incr, decr }), [incr, decr])
+    React.useImperativeHandle(fref, () => api, [api])
+
+    return (
+      <group {...props}>
+        <Center top cacheKey={JSON.stringify({ counter, font })}>
+          <Text3D bevelEnabled bevelSize={bevelSize} font={font}>
+            {counter}
+            {debug ? <meshNormalMaterial wireframe /> : <meshStandardMaterial color={color} />}
+          </Text3D>
+        </Center>
+        {children}
+      </group>
+    )
+  }
+)

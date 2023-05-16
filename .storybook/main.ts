af3b53617238c8eb3ced210423eff02e8b3510ff
Antoine BERNIER <antoine.bernier@gmail.com>
2023-05-16 12:03:33 +0200

    chore: re-enabling knobs and storysource

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   staticDirs: ['./public'],
   stories: ['./stories/**/*.stories.{ts,tsx}'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-knobs', '@storybook/addon-storysource'],
   framework: {
     name: '@storybook/react-vite',
     options: {},

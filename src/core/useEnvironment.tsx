b84964324a4500b95f7f0efd4244d10e21d705a4
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-06-02 15:26:38 -0500

    chore: restore assets helper

--- src/core/useEnvironment.tsx
+++ src/core/useEnvironment.tsx
@@ -11,28 +11,14 @@ import {
   TextureEncoding,
 } from 'three'
 import { RGBELoader } from 'three-stdlib'
+import { presetsObj, PresetsType } from '../helpers/environment-assets'
 
 const CUBEMAP_ROOT = 'https://raw.githack.com/pmndrs/drei-assets/456060a26bbeb8fdf79326f224b6d99b8bcce736'
-
-const presets = {
-  apartment: 'lebombo_1k.hdr',
-  city: 'potsdamer_platz_1k.hdr',
-  dawn: 'kiara_1_dawn_1k.hdr',
-  forest: 'forest_slope_1k.hdr',
-  lobby: 'st_fagans_interior_1k.hdr',
-  night: 'dikhololo_night_1k.hdr',
-  park: 'rooitou_park_1k.hdr',
-  studio: 'studio_small_03_1k.hdr',
-  sunset: 'venice_sunset_1k.hdr',
-  warehouse: 'empty_warehouse_01_1k.hdr',
-}
-
-export type Presets = keyof typeof presets
 
 export type EnvironmentLoaderProps = {
   files?: string | string[]
   path?: string
-  preset?: Presets
+  preset?: PresetsType
   extensions?: (loader: Loader) => void
   encoding?: TextureEncoding
 }
@@ -45,8 +31,8 @@ export function useEnvironment({
   extensions,
 }: Partial<EnvironmentLoaderProps> = {}) {
   if (preset) {
-    if (!(preset in presets)) throw new Error('Preset must be one of: ' + Object.keys(presets).join(', '))
-    files = presets[preset]
+    if (!(preset in presetsObj)) throw new Error('Preset must be one of: ' + Object.keys(presetsObj).join(', '))
+    files = presetsObj[preset]
     path = CUBEMAP_ROOT
   }
 
---
+++ src/helpers/environment-assets.ts
@@ -0,0 +1,14 @@
+export const presetsObj = {
+  apartment: 'lebombo/lebombo_1k.hdr',
+  city: 'potsdamer-platz/potsdamer_platz_1k.hdr',
+  dawn: 'kiara/kiara_1_dawn_1k.hdr',
+  forest: 'forrest-slope/forest_slope_1k.hdr',
+  lobby: 'st-fagans/st_fagans_interior_1k.hdr',
+  night: 'dikhololo/dikhololo_night_1k.hdr',
+  park: 'rooitou/rooitou_park_1k.hdr',
+  studio: 'studio-small-3/studio_small_03_1k.hdr',
+  sunset: 'venice/venice_sunset_1k.hdr',
+  warehouse: 'empty-wharehouse/empty_warehouse_01_1k.hdr',
+}
+
+export type PresetsType = keyof typeof presetsObj

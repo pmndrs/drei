0158bb7a6a6e11c72a218d01bf312e77fb49db36
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-06-02 15:24:12 -0500

    fix(useEnvironment): use Drei CDN

--- src/core/useEnvironment.tsx
+++ src/core/useEnvironment.tsx
@@ -11,14 +11,28 @@ import {
   TextureEncoding,
 } from 'three'
 import { RGBELoader } from 'three-stdlib'
-import { presetsObj, PresetsType } from '../helpers/environment-assets'
 
-const CUBEMAP_ROOT = 'https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/hdris/'
+const CUBEMAP_ROOT = 'https://raw.githack.com/pmndrs/drei-assets/456060a26bbeb8fdf79326f224b6d99b8bcce736'
+
+const presets = {
+  apartment: 'lebombo_1k.hdr',
+  city: 'potsdamer_platz_1k.hdr',
+  dawn: 'kiara_1_dawn_1k.hdr',
+  forest: 'forest_slope_1k.hdr',
+  lobby: 'st_fagans_interior_1k.hdr',
+  night: 'dikhololo_night_1k.hdr',
+  park: 'rooitou_park_1k.hdr',
+  studio: 'studio_small_03_1k.hdr',
+  sunset: 'venice_sunset_1k.hdr',
+  warehouse: 'empty_warehouse_01_1k.hdr',
+}
+
+export type Presets = keyof typeof presets
 
 export type EnvironmentLoaderProps = {
   files?: string | string[]
   path?: string
-  preset?: PresetsType
+  preset?: Presets
   extensions?: (loader: Loader) => void
   encoding?: TextureEncoding
 }
@@ -31,8 +45,8 @@ export function useEnvironment({
   extensions,
 }: Partial<EnvironmentLoaderProps> = {}) {
   if (preset) {
-    if (!(preset in presetsObj)) throw new Error('Preset must be one of: ' + Object.keys(presetsObj).join(', '))
-    files = presetsObj[preset]
+    if (!(preset in presets)) throw new Error('Preset must be one of: ' + Object.keys(presets).join(', '))
+    files = presets[preset]
     path = CUBEMAP_ROOT
   }
 

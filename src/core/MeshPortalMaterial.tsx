a18f236db4c959a52828a49b2ee2c3babf194ad1
— <drcmda@gmail.com>
2023-06-05 15:59:25 +0200

    feat: portal

---
+++ src/core/MeshPortalMaterial.tsx
@@ -0,0 +1,64 @@
+import * as THREE from 'three'
+import * as React from 'react'
+import { ReactThreeFiber, extend, useFrame, useThree } from '@react-three/fiber'
+import { RenderTexture } from './RenderTexture'
+import { shaderMaterial } from './shaderMaterial'
+
+const PortalMaterial = shaderMaterial(
+  {
+    map: null,
+    resolution: new THREE.Vector2(),
+  },
+  `void main() {
+     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+   }`,
+  `uniform sampler2D map;
+   uniform vec2 resolution;
+   void main() {
+     vec2 uv = gl_FragCoord.xy / resolution.xy;
+     gl_FragColor = texture2D(map, uv);
+     #include <tonemapping_fragment>
+     #include <encodings_fragment>
+   }`
+)
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      portalMaterial: ReactThreeFiber.ShaderMaterialProps & { resolution: ReactThreeFiber.Vector2 }
+    }
+  }
+}
+
+export type PortalProps = JSX.IntrinsicElements['mesh']
+
+export const MeshPortalMaterial = React.forwardRef(
+  ({ children, ...props }: PortalProps, fref: React.ForwardedRef<typeof PortalMaterial>) => {
+    extend({ PortalMaterial })
+    const ref = React.useRef<typeof PortalMaterial>(null!)
+    const group = React.useRef<THREE.Group>(null!)
+    const { size, events, viewport } = useThree()
+    useFrame(() => {
+      let parent = (ref.current as any)?.__r3f.parent
+      if (parent) {
+        group.current.matrix.copy(parent.matrixWorld)
+      }
+    })
+    React.useImperativeHandle(fref, () => ref.current)
+    return (
+      <portalMaterial
+        // @ts-ignore
+        ref={ref}
+        resolution={[size.width * viewport.dpr, size.height * viewport.dpr]}
+        toneMapped={false}
+        {...props}
+      >
+        <RenderTexture attach="map" compute={events.compute as any}>
+          <group onPointerOver={() => null} matrixAutoUpdate={false} ref={group}>
+            {children}
+          </group>
+        </RenderTexture>
+      </portalMaterial>
+    )
+  }
+)

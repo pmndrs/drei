79e6ed64c72ab742f5ba338a59a365bf66e153ab
Michael Dobekidis <m.dobekidis@7linternational.com>
2023-06-19 13:02:50 +0300

    feat: extended gradient texture with radial variation

--- src/core/GradientTexture.tsx
+++ src/core/GradientTexture.tsx
@@ -1,27 +1,70 @@
 import * as React from 'react'
+import { useThree } from '@react-three/fiber'
+export enum GradientType {
+  Linear = 'linear',
+  Radial = 'radial',
+}
 
 type Props = {
   stops: Array<number>
   colors: Array<string>
   attach?: string
   size?: number
+  width?: number
+  type?: GradientType
+  innerCircleRadius?: number
+  outerCircleRadius?: string | number
 } & JSX.IntrinsicElements['texture']
 
-export function GradientTexture({ stops, colors, size = 1024, ...props }: Props) {
+export function GradientTexture({
+  stops,
+  colors,
+  size = 1024,
+  width = 16,
+  //@ts-ignore - weird error about type never, although the type is clearly defined
+  type = GradientType.Linear,
+  innerCircleRadius = 0,
+  outerCircleRadius = 'auto',
+  ...props
+}: Props) {
+  const gl = useThree((state: any) => state.gl)
   const canvas: HTMLCanvasElement = React.useMemo(() => {
     const canvas = document.createElement('canvas')
     const context = canvas.getContext('2d')!
-    canvas.width = 16
+    canvas.width = width
     canvas.height = size
-    const gradient = context.createLinearGradient(0, 0, 0, size)
+    let gradient
+    if (type === GradientType.Linear) {
+      gradient = context.createLinearGradient(0, 0, 0, size)
+    } else {
+      const canvasCenterX = canvas.width / 2
+      const canvasCenterY = canvas.height / 2
+      const radius =
+        outerCircleRadius !== 'auto'
+          ? Math.abs(Number(outerCircleRadius))
+          : Math.sqrt(canvasCenterX ** 2 + canvasCenterY ** 2)
+      gradient = context.createRadialGradient(
+        canvasCenterX,
+        canvasCenterY,
+        Math.abs(innerCircleRadius),
+        canvasCenterX,
+        canvasCenterY,
+        radius
+      )
+    }
+
     let i = stops.length
     while (i--) {
       gradient.addColorStop(stops[i], colors[i])
     }
+    context.save()
     context.fillStyle = gradient
-    context.fillRect(0, 0, 16, size)
+    context.fillRect(0, 0, width, size)
+    context.restore()
+
     return canvas
   }, [stops])
+
   // @ts-ignore ????
-  return <canvasTexture args={[canvas]} attach="map" {...props} />
+  return <canvasTexture colorSpace={gl.outputColorSpace} args={[canvas]} attach="map" {...props} />
 }

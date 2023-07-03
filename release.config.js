92b96f56da02a92a342f5802adf076f3046e226d
Antoine BERNIER <antoine.bernier@gmail.com>
2023-07-03 21:59:24 +0200

    chore: `canary-*` (#1555)
    * chore: canary releases support
    
    * fix: gh actions

--- release.config.js
+++ release.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  branches: ['master', { name: 'alpha', prerelease: true }, { name: 'beta', prerelease: true }],
+  branches: [
+    'master',
+    { name: 'alpha', prerelease: true },
+    { name: 'beta', prerelease: true },
+    { name: 'canary-*', prerelease: true, channel: 'canary' },
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',

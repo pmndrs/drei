3bc30f1651004c40bd6536f3ab97b3cddaca68e9
Michalis Dobekidis <m.dobekidis@7Linternational.com>
2023-08-24 17:02:11 +0300

    fix: added bevel-segments property

--- src/core/RoundedBox.tsx
+++ src/core/RoundedBox.tsx
@@ -19,6 +19,7 @@ type Props = {
   args?: NamedArrayTuple<(width?: number, height?: number, depth?: number) => void>
   radius?: number
   smoothness?: number
+  bevelSegments?: number
   steps?: number
   creaseAngle?: number
 } & Omit<JSX.IntrinsicElements['mesh'], 'args'>

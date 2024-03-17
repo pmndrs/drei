83218cabf5abb164e9445196715bd34c90d25d01
Cody Bennett <hi@codyb.co>
2024-03-16 20:25:47 -0500

    chore: revert TS changes

--- src/core/Stats.tsx
+++ src/core/Stats.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { addEffect, addAfterEffect } from '@react-three/fiber'
+// @ts-ignore
 import StatsImpl from 'stats.js'
 import { useEffectfulState } from '../helpers/useEffectfulState'
 

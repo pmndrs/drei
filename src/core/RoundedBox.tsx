f9167bfd9f7eb86f5e0d49c8864e70a4f7585538
Riku-KANO <rickwindows0127@gmail.com>
2024-04-13 14:54:19 +0900

    fix: add @__PURE__ annotation

--- src/core/RoundedBox.tsx
+++ src/core/RoundedBox.tsx
@@ -19,27 +19,36 @@ type Props = {
   creaseAngle?: number
 } & Omit<JSX.IntrinsicElements['mesh'], 'args'>
 
-export const RoundedBox: ForwardRefComponent<Props, Mesh> = React.forwardRef<Mesh, Props>(function RoundedBox(
-  { args: [width = 1, height = 1, depth = 1] = [], segments = 4, radius = 0.05, creaseAngle = 0.4, children, ...rest },
-  ref
-) {
-  React.useMemo(() => extend({ RoundedBoxGeometry }), [])
+export const RoundedBox: ForwardRefComponent<Props, Mesh> = /* @__PURE__ */ React.forwardRef<Mesh, Props>(
+  function RoundedBox(
+    {
+      args: [width = 1, height = 1, depth = 1] = [],
+      segments = 4,
+      radius = 0.05,
+      creaseAngle = 0.4,
+      children,
+      ...rest
+    },
+    ref
+  ) {
+    React.useMemo(() => extend({ RoundedBoxGeometry }), [])
 
-  const geomRef = React.useRef<RoundedBoxGeometry>(null!)
+    const geomRef = React.useRef<RoundedBoxGeometry>(null!)
 
-  React.useLayoutEffect(() => {
-    if (geomRef.current) {
-      geomRef.current.center()
-      toCreasedNormals(geomRef.current, creaseAngle)
-    }
-  }, [geomRef])
+    React.useLayoutEffect(() => {
+      if (geomRef.current) {
+        geomRef.current.center()
+        toCreasedNormals(geomRef.current, creaseAngle)
+      }
+    }, [geomRef])
 
-  const args = React.useMemo(() => [width, height, depth, segments, radius], [width, height, depth, segments, radius])
+    const args = React.useMemo(() => [width, height, depth, segments, radius], [width, height, depth, segments, radius])
 
-  return (
-    <mesh ref={ref} {...rest}>
-      <roundedBoxGeometry ref={geomRef} args={args} />
-      {children}
-    </mesh>
-  )
-})
+    return (
+      <mesh ref={ref} {...rest}>
+        <roundedBoxGeometry ref={geomRef} args={args} />
+        {children}
+      </mesh>
+    )
+  }
+)

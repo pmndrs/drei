0b99d515b745cace4cb27a162f1e3ae2ef77aa91
Antoine BERNIER <antoine.bernier@gmail.com>
2024-05-31 23:47:03 +0200

    chore: npx storybook automigrate (#1967)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,13 +3,17 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   staticDirs: ['./public'],
   stories: ['./stories/**/*.stories.{ts,tsx}'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@chromatic-com/storybook'],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  docs: {
-    autodocs: true,
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
   },
 }
 

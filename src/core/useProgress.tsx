5d6979ff5900cded3d1830ac22cf0a22e7e016d0
Mayank <mayank.srmu@gmail.com>
2024-06-04 05:02:37 +0000

    fix: add treeshakable to useProgress
    This will ensure that only one instance of useProgess is created.
    For more info please see https://github.com/react18-tools/treeshakable/ and https://treeshakable.vercel.app/

--- src/core/useProgress.tsx
+++ src/core/useProgress.tsx
@@ -1,4 +1,5 @@
 import { DefaultLoadingManager } from 'three'
+import treeshakable from 'treeshakable'
 import create from 'zustand'
 
 type Data = {
@@ -11,40 +12,43 @@ type Data = {
 }
 let saveLastTotalLoaded = 0
 
-const useProgress = /* @__PURE__ */ create<Data>((set) => {
-  DefaultLoadingManager.onStart = (item, loaded, total) => {
-    set({
-      active: true,
-      item,
-      loaded,
-      total,
-      progress: ((loaded - saveLastTotalLoaded) / (total - saveLastTotalLoaded)) * 100,
-    })
-  }
-  DefaultLoadingManager.onLoad = () => {
-    set({ active: false })
-  }
-  DefaultLoadingManager.onError = (item) => set((state) => ({ errors: [...state.errors, item] }))
-  DefaultLoadingManager.onProgress = (item, loaded, total) => {
-    if (loaded === total) {
-      saveLastTotalLoaded = total
+const createUseProgress = () =>
+  /* @__PURE__ */ create<Data>((set) => {
+    DefaultLoadingManager.onStart = (item, loaded, total) => {
+      set({
+        active: true,
+        item,
+        loaded,
+        total,
+        progress: ((loaded - saveLastTotalLoaded) / (total - saveLastTotalLoaded)) * 100,
+      })
     }
-    set({
-      active: true,
-      item,
-      loaded,
-      total,
-      progress: ((loaded - saveLastTotalLoaded) / (total - saveLastTotalLoaded)) * 100 || 100,
-    })
-  }
-  return {
-    errors: [],
-    active: false,
-    progress: 0,
-    item: '',
-    loaded: 0,
-    total: 0,
-  }
-})
+    DefaultLoadingManager.onLoad = () => {
+      set({ active: false })
+    }
+    DefaultLoadingManager.onError = (item) => set((state) => ({ errors: [...state.errors, item] }))
+    DefaultLoadingManager.onProgress = (item, loaded, total) => {
+      if (loaded === total) {
+        saveLastTotalLoaded = total
+      }
+      set({
+        active: true,
+        item,
+        loaded,
+        total,
+        progress: ((loaded - saveLastTotalLoaded) / (total - saveLastTotalLoaded)) * 100 || 100,
+      })
+    }
+    return {
+      errors: [],
+      active: false,
+      progress: 0,
+      item: '',
+      loaded: 0,
+      total: 0,
+    }
+  })
+
+const useProgress = treeshakable('drei-useProgress', createUseProgress)
 
 export { useProgress }

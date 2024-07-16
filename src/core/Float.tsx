4bfbff74da04cd0f25d44708d40554f2b8148bd6
= <aaronclaes22@gmail.com>
2024-07-16 21:14:09 +0200

    fix(Float): add invalidate to useFrame for on-demand rendering

--- src/core/Float.tsx
+++ src/core/Float.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useFrame } from '@react-three/fiber'
+import { invalidate, useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import { ForwardRefComponent } from '../helpers/ts-utils'
 
@@ -33,6 +33,8 @@ export const Float: ForwardRefComponent<FloatProps, THREE.Group> = /* @__PURE__ 
     const offset = React.useRef(Math.random() * 10000)
     useFrame((state) => {
       if (!enabled || speed === 0) return
+
+      invalidate()
       const t = offset.current + state.clock.getElapsedTime()
       ref.current.rotation.x = (Math.cos((t / 4) * speed) / 8) * rotationIntensity
       ref.current.rotation.y = (Math.sin((t / 4) * speed) / 8) * rotationIntensity

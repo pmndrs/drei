71864076c35c3b4f201cd9518da873eff7dc90a1
Hichem Fantar <hichemfantar2049@gmail.com>
2024-09-24 13:34:01 +0100

    chore: Remove glslify plugin from vite.config.js and add it to vite.config.ts (#2109)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import glslify from 'vite-plugin-glslify'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), glslify()],
 })

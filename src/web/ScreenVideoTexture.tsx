b58201b9cb1d22dc2e271a5f81b67d4506794137
Cody Bennett <hi@codyb.co>
2024-12-31 03:16:41 -0600

    fix: remove use of ambient namespace THREE

--- src/web/ScreenVideoTexture.tsx
+++ src/web/ScreenVideoTexture.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import * as THREE from 'three'
 import { forwardRef, useEffect } from 'react'
 import { suspend, clear } from 'suspend-react'
 import { VideoTexture, VideoTextureProps } from '..'

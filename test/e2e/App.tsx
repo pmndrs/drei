44ccb323c24b10fd65567d03e4f16b3eee5960c2
Cody Bennett <hi@codyb.co>
2025-01-10 06:25:17 -0600

    experiment: set arbitrary delay in e2e

--- test/e2e/App.tsx
+++ test/e2e/App.tsx
@@ -18,7 +18,7 @@ function App() {
 
 function Scene() {
   useEffect(() => {
-    requestAnimationFrame(() => requestAnimationFrame(() => document.dispatchEvent(new Event('playright:r3f'))))
+    setTimeout(() => document.dispatchEvent(new Event('playright:r3f')), 10000)
   }, [])
 
   return (

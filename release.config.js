589311ac766b6f1788e63b12f7dce6f67eaceace
Antoine BERNIER <antoine.bernier@gmail.com>
2025-01-16 13:41:55 +0100

    chore: rc branch

--- release.config.js
+++ release.config.js
@@ -3,7 +3,8 @@ module.exports = {
     'master',
     { name: 'alpha', prerelease: true },
     { name: 'beta', prerelease: true },
-    { name: 'canary-*', prerelease: true, channel: 'rc' },
+    { name: 'rc', prerelease: true },
+    { name: 'canary-*', prerelease: true, channel: 'canary' },
   ],
   plugins: [
     '@semantic-release/commit-analyzer',

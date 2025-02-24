a3ba6f9f554425062521de38f4341fc93bf3a16b
Cody Bennett <hi@codyb.co>
2025-02-24 11:48:21 -0600

    fix(Merged): restores recursive implementation (#2366)

---
+++ .storybook/stories/Merged.stories.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Setup } from '../Setup'
+
+import { useGLTF, Merged, Instance } from '../../src'
+
+export default {
+  title: 'Performance/Merged',
+  component: Merged,
+  decorators: [
+    (Story) => (
+      <Setup>
+        <Story />
+      </Setup>
+    ),
+  ],
+} satisfies Meta<typeof Merged>
+
+type Story = StoryObj<typeof Merged>
+
+function Scene() {
+  const { nodes } = useGLTF('suzanne.glb', true)
+  return <Merged meshes={nodes}>{({ Suzanne }) => <Suzanne />}</Merged>
+}
+
+export const DefaultStory = {
+  render: (args) => <Scene {...args} />,
+  name: 'Default',
+} satisfies Story

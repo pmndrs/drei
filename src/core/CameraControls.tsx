f57347481b92b17319347cc625f5d274e120053c
Antoine BERNIER <antoine.bernier@gmail.com>
2025-06-15 23:40:48 +0200

    CameraControls[impl] subclass

--- src/core/CameraControls.tsx
+++ src/core/CameraControls.tsx
@@ -22,10 +22,15 @@ import { extend, useFrame, useThree, ReactThreeFiber, EventManager, ThreeElement
 import CameraControlsImpl from 'camera-controls'
 import { ForwardRefComponent, Overwrite } from '../helpers/ts-utils'
 
+type CameraControlsClass = {
+  new (...args: ConstructorParameters<typeof CameraControlsImpl>): CameraControlsImpl
+} & typeof CameraControlsImpl
+
 export type CameraControlsProps = Omit<
   Overwrite<
     ThreeElement<typeof CameraControlsImpl>,
     {
+      impl?: CameraControlsClass
       camera?: PerspectiveCamera | OrthographicCamera
       domElement?: HTMLElement
       makeDefault?: boolean
@@ -57,6 +62,28 @@ export const CameraControls: ForwardRefComponent<CameraControlsProps, CameraCont
   CameraControlsImpl,
   CameraControlsProps
 >((props, ref) => {
+  const {
+    impl: SubclassImpl,
+    camera,
+    domElement,
+    makeDefault,
+    onControlStart,
+    onControl,
+    onControlEnd,
+    onTransitionStart,
+    onUpdate,
+    onWake,
+    onRest,
+    onSleep,
+    onStart,
+    onEnd,
+    onChange,
+    regress,
+    ...restProps
+  } = props
+
+  const Impl = SubclassImpl ?? CameraControlsImpl
+
   // useMemo is used here instead of useEffect, otherwise the useMemo below runs first and throws
   useMemo(() => {
     // to allow for tree shaking, we only import the subset of THREE that is used by camera-controls
@@ -76,28 +103,9 @@ export const CameraControls: ForwardRefComponent<CameraControlsProps, CameraCont
       Vector4,
     }
 
-    CameraControlsImpl.install({ THREE: subsetOfTHREE })
-    extend({ CameraControlsImpl })
-  }, [])
-
-  const {
-    camera,
-    domElement,
-    makeDefault,
-    onControlStart,
-    onControl,
-    onControlEnd,
-    onTransitionStart,
-    onUpdate,
-    onWake,
-    onRest,
-    onSleep,
-    onStart,
-    onEnd,
-    onChange,
-    regress,
-    ...restProps
-  } = props
+    Impl.install({ THREE: subsetOfTHREE })
+    extend({ Impl })
+  }, [Impl])
 
   const defaultCamera = useThree((state) => state.camera)
   const gl = useThree((state) => state.gl)
@@ -111,7 +119,7 @@ export const CameraControls: ForwardRefComponent<CameraControlsProps, CameraCont
   const explCamera = camera || defaultCamera
   const explDomElement = (domElement || events.connected || gl.domElement) as HTMLElement
 
-  const controls = useMemo(() => new CameraControlsImpl(explCamera), [explCamera])
+  const controls = useMemo(() => new Impl(explCamera), [Impl, explCamera])
 
   useFrame((state, delta) => {
     controls.update(delta)

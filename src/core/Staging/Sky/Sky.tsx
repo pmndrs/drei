03eeb86661cc3482dd89dabdb2f14aaa1b13ce0a
Dennis Smolek <desopolis@gmail.com>
2026-01-05 00:32:53 +0900

    feat: webgpu sky

--- src/core/Staging/Sky/Sky.tsx
+++ src/core/Staging/Sky/Sky.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
-import { ReactThreeFiber } from '@react-three/fiber'
+import { applyProps, ReactThreeFiber, useThree } from '@react-three/fiber'
 import { Sky as SkyImpl } from 'three/examples/jsm/objects/Sky.js'
+// webgpu
+import { SkyMesh } from 'three/examples/jsm/objects/SkyMesh.js'
 import { Vector3 } from '#three'
 import { ForwardRefComponent } from '../../../utils/ts-utils'
 
@@ -26,6 +28,15 @@ export function calcPosFromAngles(inclination: number, azimuth: number, vector: 
   return vector
 }
 
+//* Helper to convert R3F Vector3 prop to Vector3 instance ==============================
+function toVector3(value: ReactThreeFiber.Vector3): Vector3 {
+  if (value instanceof Vector3) return value
+  if (Array.isArray(value)) return new Vector3(value[0], value[1], value[2])
+  if (typeof value === 'number') return new Vector3(value, value, value)
+  // Object with x, y, z properties
+  return new Vector3((value as any).x, (value as any).y, (value as any).z)
+}
+
 export const Sky: ForwardRefComponent<SkyProps, SkyImpl> = /* @__PURE__ */ React.forwardRef(
   (
     {
@@ -41,16 +52,56 @@ export const Sky: ForwardRefComponent<SkyProps, SkyImpl> = /* @__PURE__ */ React
     }: SkyProps,
     ref
   ) => {
-    const [sky] = React.useState(() => {
-      const skyInstance = new SkyImpl()
-      skyInstance.scale.setScalar(distance)
-      return skyInstance
-    })
+    // detect if legacy or webgpu
+    const { isLegacy } = useThree()
+
+    const sky = React.useMemo(() => {
+      if (isLegacy) {
+        const skyInstance = new SkyImpl()
+        skyInstance.scale.setScalar(distance)
+        return skyInstance
+      } else {
+        const skyInstance = new SkyMesh()
+        skyInstance.scale.setScalar(distance)
+        return skyInstance
+      }
+    }, [isLegacy, distance])
 
     React.useLayoutEffect(() => {
       sky.scale.setScalar(distance)
     }, [sky, distance])
 
+    // Apply sky uniforms ---------------------------------
+    React.useLayoutEffect(() => {
+      if (!sky) return
+
+      if (isLegacy) {
+        // Legacy: apply via material uniforms
+        applyProps(sky.material, {
+          mieCoefficient,
+          mieDirectionalG,
+          rayleigh,
+          sunPosition,
+          turbidity,
+        })
+      } else {
+        // WebGPU: SkyMesh has uniforms as direct properties
+        const webgpuSky = sky as SkyMesh
+        webgpuSky.turbidity.value = turbidity
+        webgpuSky.rayleigh.value = rayleigh
+        webgpuSky.mieCoefficient.value = mieCoefficient
+        webgpuSky.mieDirectionalG.value = mieDirectionalG
+
+        // Handle sunPosition - convert from R3F Vector3 format to actual Vector3
+        const sunVec = toVector3(sunPosition)
+        webgpuSky.sunPosition.value.copy(sunVec)
+      }
+    }, [sky, isLegacy, turbidity, rayleigh, mieCoefficient, mieDirectionalG, sunPosition])
+
+    // webgpu
+    if (!isLegacy) return <primitive object={sky} ref={ref} {...props} />
+
+    // legacy
     return (
       <primitive
         object={sky}

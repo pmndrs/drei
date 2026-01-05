e86a7b91b298db703e34e40ca05d18daebae115c
Antoine BERNIER <antoine.bernier@gmail.com>
2026-01-05 15:57:24 +0100

    chore(chromatic): disable anims (#2590)
    * SayCheese and deterministic random
    
    * 3000
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * 3000
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * flushSync
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import type { Preview } from '@storybook/react-vite'
+import seedrandom from 'seedrandom'
 
 import './index.css'
+
+seedrandom('deterministic-random-for-storybook', { global: true }) // deterministic Math.random()
 
 const preview: Preview = {
   parameters: {
     layout: 'fullscreen',
-    chromatic: { disableSnapshot: true },
   },
 
   tags: ['autodocs'],

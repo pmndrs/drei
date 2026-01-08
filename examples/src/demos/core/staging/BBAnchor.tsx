9ae4805d267d98724ecce9e9a634225d51ef44c6
Dennis Smolek <desopolis@gmail.com>
2026-01-08 16:09:18 +0900

    feat: bbAnchor

--- examples/src/demos/core/staging/BBAnchor.tsx
+++ examples/src/demos/core/staging/BBAnchor.tsx
@@ -1,6 +1,9 @@
-import { BBAnchor, OrbitControls, Text } from '@react-three/drei/core'
+import { BBAnchor, Html, Icosahedron, OrbitControls, Text, Text3D, useHelper } from '@react-three/drei/core'
 import { CanvasWithToggle } from '@ex/components/PlatformSwitch'
 import { ExampleCard } from '../../../components/ExampleCard'
+import { ComponentProps } from 'react'
+import { Mesh, BoxHelper, IcosahedronGeometry } from 'three'
+import { useRef } from 'react'
 
 //* BBAnchor Demo ==============================
 
@@ -13,26 +16,35 @@ function Scene() {
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1} />
 
-      {/* Reference mesh */}
-      <mesh>
-        <boxGeometry args={[2, 1, 1]} />
-        <meshStandardMaterial color="hotpink" wireframe />
-      </mesh>
-
-      {/* BBAnchor positions text relative to bounding box */}
-      <BBAnchor anchor={[0, 1, 0]}>
-        <Text position={[0, 0.2, 0]} fontSize={0.25} color="white">
-          Top
-        </Text>
-      </BBAnchor>
-
-      <BBAnchor anchor={[0, -1, 0]}>
-        <Text position={[0, -0.2, 0]} fontSize={0.25} color="white">
-          Bottom
-        </Text>
-      </BBAnchor>
+      <BBAnchorScene />
 
       <gridHelper args={[10, 10, '#444', '#333']} position={[0, -2, 0]} />
+    </>
+  )
+}
+
+function BBAnchorScene() {
+  const ref = useRef<Mesh<IcosahedronGeometry>>(null!)
+
+  useHelper(ref, BoxHelper, 'cyan')
+
+  return (
+    <>
+      <OrbitControls autoRotate />
+      <Icosahedron ref={ref}>
+        <meshBasicMaterial color="hotpink" wireframe />
+        <BBAnchor anchor={[1, 1, 1]}>
+          <Html
+            style={{
+              color: 'white',
+              whiteSpace: 'nowrap',
+            }}
+            center
+          >
+            Html element
+          </Html>
+        </BBAnchor>
+      </Icosahedron>
     </>
   )
 }

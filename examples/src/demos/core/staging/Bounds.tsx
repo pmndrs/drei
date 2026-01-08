3984ddbaef7f091ef7bac0f61bb8839075fc5427
Dennis Smolek <desopolis@gmail.com>
2026-01-08 16:20:52 +0900

    feat: bounds demo

--- examples/src/demos/core/staging/Bounds.tsx
+++ examples/src/demos/core/staging/Bounds.tsx
@@ -1,14 +1,39 @@
-import { Bounds, OrbitControls } from '@react-three/drei/core'
+import { Bounds, OrbitControls, useBounds } from '@react-three/drei/core'
 import { CanvasWithToggle } from '@ex/components/PlatformSwitch'
 import { ExampleCard } from '../../../components/ExampleCard'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { Mesh, SphereGeometry } from 'three'
 
 //* Bounds Demo ==============================
 
-function Scene() {
+function BoundsSphere({ pointerMissedCount }: { pointerMissedCount: number }) {
+  const sphereRef = useRef<Mesh<SphereGeometry>>(null!)
+
+  const bounds = useBounds()
+
+  const handleClick = useCallback(() => {
+    bounds.refresh(sphereRef.current).clip().fit()
+  }, [bounds])
+
+  // when offclick happens reset the bounds
+  useEffect(() => {
+    if (pointerMissedCount > 0) {
+      console.log('resetting bounds')
+      bounds.reset()
+    }
+  }, [pointerMissedCount, bounds])
+
+  return (
+    <mesh ref={sphereRef} onClick={handleClick}>
+      <sphereGeometry args={[0.8, 32, 32]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  )
+}
+
+function Scene({ pointerMissedCount }: { pointerMissedCount: number }) {
   return (
     <>
-      <OrbitControls makeDefault />
-
       {/* Lighting */}
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1} />
@@ -20,10 +45,7 @@ function Scene() {
           <meshStandardMaterial color="hotpink" />
         </mesh>
 
-        <mesh position={[2, 0, 0]}>
-          <sphereGeometry args={[0.8, 32, 32]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
+        <BoundsSphere pointerMissedCount={pointerMissedCount} />
       </Bounds>
 
       <gridHelper args={[10, 10, '#444', '#333']} position={[0, -2, 0]} />
@@ -32,13 +54,18 @@ function Scene() {
 }
 
 export default function BoundsDemo() {
+  const [pointerMissedCount, setPointerMissedCount] = useState(0)
+
+  const handlePointerMissed = useCallback(() => {
+    setPointerMissedCount(pointerMissedCount + 1)
+  }, [pointerMissedCount])
   return (
     <div className="demo-container">
       <ExampleCard demoName="Bounds" />
 
       <div className="demo-canvas">
-        <CanvasWithToggle camera={{ position: [0, 0, 5], fov: 50 }}>
-          <Scene />
+        <CanvasWithToggle camera={{ position: [0, 0, 5], fov: 50 }} onPointerMissed={handlePointerMissed}>
+          <Scene pointerMissedCount={pointerMissedCount} />
         </CanvasWithToggle>
       </div>
     </div>

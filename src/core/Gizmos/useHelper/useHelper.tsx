248b372f3823de042e4d11e02f20d50d798b6c91
Dennis Smolek <desopolis@gmail.com>
2026-02-10 17:21:52 +0900

    fix: refs can be null in useHelper implements #2529

--- src/core/Gizmos/useHelper/useHelper.tsx
+++ src/core/Gizmos/useHelper/useHelper.tsx
@@ -22,7 +22,7 @@ type HelperArgs<T> = T extends [infer _, ...infer R] ? R : never
  * ```
  */
 export function useHelper<T extends HelperConstructor>(
-  object3D: React.RefObject<Object3D> | Falsey,
+  object3D: React.RefObject<Object3D | null> | Falsey,
   helperConstructor: T,
   ...args: HelperArgs<ConstructorParameters<T>>
 ) {
